1c1aa4d6ac8d32c3243b541c61f2bab08cec2fe4
진주원(JooWon Jin) <84346055+jinjoo-lab@users.noreply.github.com>
2023-06-23 21:25:52 +0900

    [Gold IV] Title: 네트워크 연결, Time: 556 ms, Memory: 49956 KB -BaekjoonHub

---
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
@@ -0,0 +1,80 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n = 0;
+    static int m = 0;
+
+    static int[] root;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        n = Integer.parseInt(st.nextToken());
+
+        root = new int[n+1];
+        for(int i=1;i<=n;i++)
+        {
+            root[i] = i;
+        }
+
+        st = new StringTokenizer(br.readLine(), " ");
+        m = Integer.parseInt(st.nextToken());
+
+        int[][] graph = new int[m+1][3];
+
+        for(int i=1;i<=m;i++)
+        {
+            st = new StringTokenizer(br.readLine(), " ");
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+
+            graph[i][0] = start;
+            graph[i][1] = end;
+            graph[i][2] = cost;
+
+        }
+
+        kruskal(graph);
+        br.close();
+    }
+    static void kruskal(int[][] graph)
+    {
+        int cost = 0;
+        Arrays.sort(graph , (x,y) -> (x[2] - y[2]));
+
+
+        for(int i=1;i<=m;i++)
+        {
+            if(find(graph[i][0]) != find(graph[i][1]))
+            {
+                union(graph[i][0] , graph[i][1]);
+                cost += graph[i][2];
+            }
+        }
+
+        System.out.println(cost);
+    }
+
+    static int find(int x)
+    {
+        if(root[x] == x)
+            return x;
+
+        else{
+            return root[x] = find(root[x]);
+        }
+    }
+
+    static void union(int x,int y)
+    {
+        x = find(x);
+        y = find(y);
+
+        if(x < y)
+            root[y] = x;
+
+        else
+            root[x] = y;
+    }
+}
