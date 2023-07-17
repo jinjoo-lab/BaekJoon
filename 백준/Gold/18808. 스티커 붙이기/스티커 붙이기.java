669bbcfa2a94d766298e988a32f2b1ba1090e0c4
진주원(JooWon Jin) <84346055+jinjoo-lab@users.noreply.github.com>
2023-07-17 15:07:20 +0900

    [Gold III] Title: 스티커 붙이기, Time: 176 ms, Memory: 15596 KB -BaekjoonHub

---
+++ 백준/Gold/18808. 스티커 붙이기/스티커 붙이기.java
@@ -0,0 +1,193 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int n = 0;
+    static int m = 0;
+    static int k = 0;
+    static int[][] board;
+
+    static Sticker[] sarr;
+
+    static boolean[] keep;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+
+        board = new int[n+1][m+1];
+        sarr = new Sticker[k+1];
+        keep = new boolean[k+1];
+
+        for(int i=1;i<=k;i++)
+        {
+            st = new StringTokenizer(br.readLine()," ");
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            Sticker s = new Sticker(x,y);
+            for(int j=1;j<=x;j++)
+            {
+                st = new StringTokenizer(br.readLine()," ");
+                for(int a = 1; a<= y; a++)
+                {
+                    s.board[j][a] = Integer.parseInt(st.nextToken());
+                }
+            }
+
+            sarr[i] = s;
+        }
+
+
+        for(int i=1;i<=k;i++)
+        {
+            go(i);
+        }
+        System.out.println(count());
+
+        br.close();
+    }
+    static int count()
+    {
+        int result = 0;
+
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(board[i][j] == 1)
+                    result = result + 1;
+            }
+        }
+
+        return result;
+    }
+
+    static void go(int cur)
+    {
+        Sticker tmp = sarr[cur];
+
+        for(int a=0;a<4;a++)
+        {
+            if(a!=0)
+            {
+                tmp = turn(tmp);
+            }
+
+            for(int i = 1;i <= n; i++)
+            {
+                for(int j=1 ; j<= m ;j++)
+                {
+                    if(canPost(tmp,i,j))
+                    {
+                        post(tmp,i,j);
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
+    static void print(int[][] board,int x,int y)
+    {
+        for(int i=1;i<=x;i++)
+        {
+            for(int j=1;j<=y;j++)
+                System.out.print(board[i][j]+" ");
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    static boolean canPost(Sticker cur,int x,int y)
+    {
+        if(x + cur.x -1 > n)
+            return false;
+
+        if(y + cur.y - 1 > m)
+            return false;
+
+        int xdx = 1;
+        for(int i=x;i<x+cur.x;i++)
+        {
+            int ydx = 1;
+            for(int j=y;j<y+cur.y;j++)
+            {
+                if(cur.board[xdx][ydx] == 1 && board[i][j] == 1)
+                    return false;
+                ydx = ydx + 1;
+            }
+            xdx = xdx + 1;
+        }
+        return true;
+    }
+
+    static void post(Sticker cur,int x,int y)
+    {
+        int xdx = 1;
+        for(int i=x;i<x+cur.x;i++)
+        {
+            int ydx = 1;
+            for(int j=y;j<y+cur.y;j++)
+            {
+                if(board[i][j] == 0 && cur.board[xdx][ydx]==1)
+                    board[i][j] = cur.board[xdx][ydx];
+                ydx = ydx + 1;
+            }
+
+            xdx = xdx + 1;
+        }
+
+    }
+
+    static Sticker turn(Sticker cur)
+    {
+       Sticker tmp = new Sticker(cur.y,cur.x);
+       int sy = cur.x;
+       int sx = 1;
+       for(int i=1 ; i<= cur.x ; i++)
+       {
+           sx = 1;
+           for(int j= 1;j <= cur.y ; j++)
+           {
+               tmp.board[sx][sy]= cur.board[i][j];
+               sx = sx + 1;
+           }
+           sy = sy - 1;
+       }
+
+       return tmp;
+    }
+
+    static void print()
+    {
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                System.out.print(board[i][j]+" ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+
+}
+class Sticker
+{
+    int x;
+    int y;
+    int[][] board;
+
+    Sticker(int x,int y)
+    {
+        this.x = x;
+        this.y = y;
+        this.board = new int[x+1][y+1];
+    }
+}
