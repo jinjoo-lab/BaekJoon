8a0f722413fcb41ac4602939750742461d1452e6
진주원(JooWon Jin) <84346055+jinjoo-lab@users.noreply.github.com>
2023-07-24 23:36:57 +0900

    [Gold III] Title: 선물 전달, Time: 148 ms, Memory: 22092 KB -BaekjoonHub

--- 백준/Gold/1947. 선물 전달/선물 전달.java
+++ 백준/Gold/1947. 선물 전달/선물 전달.java
@@ -1,22 +1,28 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-
-    static long[] dp = new long[10000000];
-
     static int n = 0;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
         n = Integer.parseInt(st.nextToken());
+        long[] dp = new long[n+5];
 
         dp[1] = 0;
         dp[2] = 1;
 
-        for(int i = 3;i<=n;i++)
+        for(int i=3;i<=n;i++)
         {
-            dp[i] = ((dp[i-1]+ dp[i-2])  * (i-1)) % 1000000000;
+            if(i%2==0)
+            {
+                dp[i] = ((dp[i-1] * i) % 1000000000 + 1) % 1000000000;
+            }
+
+            else
+                dp[i] = ((dp[i-1] * i) % 1000000000 - 1) % 1000000000;
         }
 
         System.out.println(dp[n]);
