706f7d18c7d6919071147e4386ad9ee108236c6e
진주원(JooWon Jin) <84346055+jinjoo-lab@users.noreply.github.com>
2023-09-14 15:01:35 +0900

    [Gold III] Title: 움직이는 미로 탈출, Time: 812 ms, Memory: 528968 KB -BaekjoonHub

---
+++ 백준/Gold/16954. 움직이는 미로 탈출/움직이는 미로 탈출.java
@@ -0,0 +1,119 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static char[][] board;
+    static boolean[][] visit;
+
+    static int[] dx = {0,0,-1,1,1,1,-1,-1,0};
+    static int[] dy = {1,-1,0,0,1,-1,1,-1,0};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        board = new char[9][9];
+        visit = new boolean[9][9];
+
+        for(int i=1;i<=8;i++){
+            String line = br.readLine();
+
+            for(int j=1;j<=8;j++){
+                board[i][j] = line.charAt(j-1);
+            }
+        }
+
+        Queue<Node> q = new LinkedList<>();
+        q.add(new Node(8,1));
+
+        while(true){
+            boolean tmp = search(q);
+
+            if(tmp){
+                System.out.println(1);
+                break;
+            }
+
+            else if(!tmp && q.size() == 0)
+            {
+                System.out.println(0);
+                break;
+            }
+
+            move();
+            
+        }
+        br.close();
+    }
+
+    static boolean search(Queue<Node> q){
+
+        int size = q.size();
+
+        while(size != 0){
+            Node cur = q.poll();
+            size--;
+
+            if(board[cur.x][cur.y] == '#')
+                continue;
+
+            if(cur.x == 1 && cur.y == 8)
+                return true;
+
+            for(int i=0;i<9;i++){
+                int nx = cur.x + dx[i];
+                int ny = cur.y + dy[i];
+
+                if(nx < 1 || nx > 8 || ny < 1 || ny > 8)
+                    continue;
+
+                if(board[nx][ny] == '#')
+                    continue;
+
+                q.add(new Node(nx,ny));
+            }
+        }
+
+        return false;
+    }
+
+    static void print(){
+        for(int i=1;i<=8;i++){
+            for(int j=1;j<=8;j++){
+                System.out.print(board[i][j]+" ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    static void move(){
+        for(int i=8;i>=1;i--){
+            for(int j=1;j<=8;j++){
+                if(board[i][j] == '#') {
+                    int nx = i + dx[3];
+                    int ny = j + dy[3];
+
+                    if (nx > 8)
+                    {
+                        board[i][j] = '.';
+                        continue;
+                    }
+
+                    else{
+                        board[nx][ny] = '#';
+                        board[i][j] = '.';
+                    }
+                }
+            }
+        }
+    }
+}
+class Node{
+    int x;
+    int y;
+
+    Node(int x,int y){
+        this.x = x;
+        this.y = y;
+    }
+}
