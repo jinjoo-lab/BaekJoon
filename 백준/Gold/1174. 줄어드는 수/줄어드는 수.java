c43082823c741c10a165c4e33a3c81d6517c9f66
진주원(JooWon Jin) <84346055+jinjoo-lab@users.noreply.github.com>
2023-11-15 21:53:25 +0900

    [Gold V] Title: 줄어드는 수, Time: 140 ms, Memory: 14360 KB -BaekjoonHub

---
+++ 백준/Gold/1174. 줄어드는 수/줄어드는 수.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n = 0;
+    static ArrayList<Long> list = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        n = Integer.parseInt(st.nextToken());
+
+        if(n > 1023){
+            System.out.println(-1);
+            return;
+        }
+
+        list.add(0l);
+        for(long i=1;i<=9;i++){
+            list.add(i);
+            go(i,1);
+        }
+
+        list.sort((x,y) -> Long.compare(x,y));
+
+
+        System.out.println(list.get(n - 1));
+
+
+        br.close();
+    }
+
+    static void go(long num,int idx){
+        if(idx > 10)
+            return;
+
+        for(int i=0;i<num % 10;i++){
+            long tmp = (num * 10) + i;
+            list.add(tmp);
+
+            go(tmp,idx+1);
+        }
+    }
+
+}
