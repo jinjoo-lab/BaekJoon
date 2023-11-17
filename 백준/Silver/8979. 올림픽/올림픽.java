a5c3890a533d3a8ec7bcc1044487937b1b99e717
진주원(JooWon Jin) <84346055+jinjoo-lab@users.noreply.github.com>
2023-11-17 21:03:16 +0900

    [Silver V] Title: 올림픽, Time: 152 ms, Memory: 14696 KB, Score: 28 point -BaekjoonHub

---
+++ 백준/Silver/8979. 올림픽/올림픽.java
@@ -0,0 +1,81 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n = 0;
+    static int m = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringBuilder sb = new StringBuilder();
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        Country[] arr = new Country[n];
+
+        for(int i=1;i<=n;i++){
+            st = new StringTokenizer(br.readLine(), " ");
+
+            int num = Integer.parseInt(st.nextToken());
+            int g = Integer.parseInt(st.nextToken());
+            int s = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            arr[i-1] = new Country(num,g,s,b);
+        }
+
+        Arrays.sort(arr,(x,y) -> {
+            if(x.g == y.g){
+
+                if(x.s == y.s)
+                    return y.b - x.b;
+
+                return y.s - x.s;
+            }
+
+            return y.g - x.g;
+        });
+
+        int num = 1;
+
+        if(arr[0].num == m){
+            System.out.println(1);
+            return;
+        }
+
+        int count = 1;
+
+        for(int i = 1; i < n ;i ++){
+            Country cur = arr[i];
+
+            if(arr[i-1].g != arr[i].g || arr[i-1].s != arr[i].s || arr[i-1].b != arr[i-1].b){
+                num += count;
+                count = 1;
+            }else{
+                count += 1;
+            }
+
+            if(cur.num == m){
+                System.out.println(num);
+                break;
+            }
+        }
+
+        br.close();
+    }
+}
+class Country{
+    int num;
+    int g;
+    int s;
+    int b;
+
+    Country(int num,int g,int s,int b){
+        this.num = num;
+        this.g = g;
+        this.s = s;
+        this.b = b;
+    }
+}
