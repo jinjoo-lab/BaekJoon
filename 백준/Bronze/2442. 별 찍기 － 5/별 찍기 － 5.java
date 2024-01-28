664ffe1a5f526da6ad6f6b604ca9e4335c18f691
진주원(JooWon Jin) <84346055+jinjoo-lab@users.noreply.github.com>
2024-01-28 21:47:59 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 320 ms, Memory: 15536 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    static int n = 0;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
+        StringBuilder sb = new StringBuilder();
+
+        n = Integer.parseInt(st.nextToken());
+
+        int count = 1;
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+
+            for(int j=1;j<=count;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+            count += 2;
+        }
+
+
+        bf.close();
+    }
+}
+
