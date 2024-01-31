3c6e13817c3416b66658d5bb935aa09f5d2042b9
진주원(JooWon Jin) <84346055+jinjoo-lab@users.noreply.github.com>
2024-01-31 14:15:59 +0900

    [Gold V] Title: 치킨 배달, Time: 184 ms, Memory: 15720 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -1,99 +1,103 @@
-import java.io.*;
 import java.util.*;
-/*
- * 백준 15686 치킨 배달
- * 치킨 거리 : 집가 가장 가까운 치킨집까지 거리
- * 도시 치킨 거리 : 치킨 거리 총
- * m개를 폐업 시킬때 치킨 거리가 가장 작을까?
- * */
+import java.io.*;
 public class Main {
+
     static int n = 0;
     static int m = 0;
-    static int result = 25000000;
-    static int count = 1;
-    static int count2 = 1;
-    static point[] chicken = new point[14];
-    static point[] home = new point[101];
-    static int[][] board = new int[51][51];
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+
+    static int[][] dis;
+
+    static boolean[] v;
+
+    static ArrayList<Node> chicken = new ArrayList<>();
+    static ArrayList<Node> home = new ArrayList<>();
+
+    static int result = Integer.MAX_VALUE;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine()," ");
 
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
 
-        // 0은 빈 칸, 1은 집, 2는 치킨집
-        for(int i=1;i<=n;i++)
-        {
-            st = new StringTokenizer(br.readLine()," ");
-            for(int j=1;j<=n;j++)
-            {
-                board[i][j] = Integer.parseInt(st.nextToken());
-                if(board[i][j]==2)
-                {
-                    chicken[count]= new point(i,j);
-                    count++;
-                }
-                if(board[i][j]==1)
-                {
-                    home[count2] = new point(i,j);
-                    count2++;
+        for(int i=1;i<=n;i++){
+            st = new StringTokenizer(bf.readLine()," ");
+
+            for(int j=1;j<=n;j++){
+                int  cur = Integer.parseInt(st.nextToken());
+
+                if(cur == 1)
+                    home.add(new Node(i,j));
+
+                else if(cur == 2){
+                    chicken.add(new Node(i,j));
                 }
             }
         }
-        travel(1,0,1);
-        System.out.println(result);
-        br.close();
-    }
-    static int minus(point x,point y)
-    {
-        int nx = Math.abs(x.x - y.x);
-        int ny = Math.abs(x.y - y.y);
 
-        return nx+ny;
-    }
-    static void travel(int k,int choose,int now)
-    {
-        if(k==count)
-        {
-            int num = 0;
-            if(choose==m) {
-                for(int i=1;i<count2;i++)
-                {
-                    int dis = 25000000;
-                    point cur = home[i];
-                    for(int j=1;j<count;j++)
-                    {
-                        if(board[chicken[j].x][chicken[j].y]==0)
-                            continue;
-                        int tmp = minus(cur,chicken[j]);
-                        if(dis>tmp) {
-                            dis = tmp;
-                        }
-                    }
-                    num = num+dis;
-                }
+        int size = chicken.size();
+        int homeSize = home.size();
 
-                if (result > num) {
-                    result = num;
-                }
+        dis = new int[homeSize+1][size+1];
+
+        for(int i=0;i<homeSize;i++){
+            for(int j=0;j<size;j++){
+                dis[i][j] = dis(home.get(i),chicken.get(j));
             }
+        }
+
+        v = new boolean[size + 1];
+
+        com(0,0,size,m);
+        System.out.println(result);
+        bf.close();
+    }
+
+    static void com(int depth,int count,int n,int r){
+
+        if(depth == r){
+
+            int total = 0;
+
+            for(int i=0;i<home.size();i++){
+
+                int d = Integer.MAX_VALUE;
+
+                for(int j=0;j<n;j++){
+                    if(v[j]){
+                        d = Math.min(d,dis[i][j]);
+                    }
+                }
+
+                total += d;
+            }
+
+
+            result = Math.min(total,result);
             return;
         }
 
-        board[chicken[now].x][chicken[now].y] = 0;
-        travel(k + 1, choose,now+1);
-        board[chicken[now].x][chicken[now].y] = 2;
-        travel(k + 1, choose + 1,now+1);
+        for(int i=count;i<n;i++){
+            if(!v[i]){
+                v[i] = true;
+                com(depth + 1,i+1,n,r);
+                v[i] = false;
+            }
+        }
 
     }
+
+    static int dis(Node tmp,Node tmp2){
+        return Math.abs(tmp.x - tmp2.x) + Math.abs(tmp.y - tmp2.y);
+    }
 }
-class point
-{
+class Node{
     int x;
     int y;
-    point(int x,int y)
-    {
+
+    Node(int x,int y){
         this.x = x;
         this.y = y;
     }
