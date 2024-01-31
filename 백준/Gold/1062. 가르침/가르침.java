e8f1b2f102d470adbec813a3aee1731923bd4639
진주원(JooWon Jin) <84346055+jinjoo-lab@users.noreply.github.com>
2024-01-31 16:09:15 +0900

    [Gold IV] Title: 가르침, Time: 184 ms, Memory: 14880 KB -BaekjoonHub

--- 백준/Gold/1062. 가르침/가르침.java
+++ 백준/Gold/1062. 가르침/가르침.java
@@ -1,86 +1,83 @@
-import java.io.*;
 import java.util.*;
-
+import java.io.*;
 public class Main {
-    static int n= 0;
-    static int k=0;
-    static int result = 0;
-    static String[] words = new String[51];
-    static boolean[] isIn = new boolean[27];
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine()," ");
-        n = Integer.parseInt(st.nextToken());
-        k = Integer.parseInt(st.nextToken());
-        for(int i=1;i<=n;i++)
-        {
-            words[i] =  br.readLine();
-        }
-        if(k<5)
-        {
-            System.out.println(0);
-        }
-        else{
-            isIn[0] = true;isIn[2]=true;isIn[8]= true;
-            isIn[13] = true; isIn[19] = true;
 
-            travel(5,0);
-            System.out.println(result);
-        }
-        br.close();
-    }
-    static boolean possible(String word)
-    {
-        char[] arr = word.toCharArray();
-        for(int i=0;i<arr.length;i++)
-        {
-            if(!isIn[arr[i]-'a'])
-            {
-                return false;
+    static int n = 0;
+    static int m = 0;
+
+    static int r = 0;
+
+    static int result = 0;
+
+    static int[] words;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine()," ");
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        words = new int[n+1];
+
+
+        for(int i=1;i<=n;i++){
+            char[] arr = bf.readLine().toCharArray();
+
+            for(int j=0 , size = arr.length; j < size ; j++){
+                words[i] |= 1 << (arr[j] - 'a');
             }
         }
-        return true;
-    }
-    static void travel(int s,int cur)
-    {
-        if(cur>=27)
-        {
+
+
+        if(m < 5) {
+            System.out.println(0);
             return;
         }
-        if(s==k)
-        {
-            int num = 0;
-            for(int i=1;i<=n;i++)
-            {
-                if(possible(words[i]))
-                {
-                    num++;
+
+        if(m == 26){
+            System.out.println(n);
+            return;
+        }
+
+        int v = (1 << 'a' - 'a') | (1 << 'n' - 'a') | 1 << ('t' - 'a') | 1 << ('i' - 'a') | 1 << ('c' - 'a');
+
+        r = m - 5;
+
+
+        go(0,0,v);
+        System.out.println(result);
+        bf.close();
+    }
+
+    static void go(int depth,int start,int v){
+
+        if(depth == r){
+
+            int count = 0;
+
+            for(int i=1;i<=n;i++){
+
+
+                if((words[i] & v) == words[i]) {
+                    count++;
                 }
             }
-            if(num>result)
-            {
-                result = num;
-            }
+
+            result = Math.max(result,count);
             return;
         }
-        if(!isIn[cur])
-        {
-            isIn[cur] = true;
-            travel(s+1,cur+1);
-            isIn[cur] = false;
-            travel(s,cur+1);
+
+
+        for(int i=start;i<26;i++){
+
+            if(i == 'a' - 'a' || i == 'n' - 'a' || i == 't' - 'a' || i == 'i' - 'a' || i == 'c' - 'a')
+                continue;
+
+            if((v & 1 << i) != 0)
+                continue;
+
+            go(depth + 1 , i+1,v | 1 << i);
         }
-        if(isAnti(cur))
-            travel(s,cur+1);
-    }
-    static boolean isAnti(int i)
-    {
-        if(i==0||i==2||i==8||i==13||i==19)
-            return true;
-        return false;
     }
 }
-/*
-*   호출 조건 : 가르친 알파벳의 수 ?
-*   탈출 조건 : 해당 수가 k에 도달했을 때
-* */
