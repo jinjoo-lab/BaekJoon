998413cc153ffca41589d428d9b94d6965fea374
진주원(JooWon Jin) <84346055+jinjoo-lab@users.noreply.github.com>
2024-02-05 12:01:20 +0900

    [Platinum V] Title: 오아시스 재결합, Time: 568 ms, Memory: 60752 KB -BaekjoonHub

---
+++ 백준/Platinum/3015. 오아시스 재결합/오아시스 재결합.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int[] board;
+
+    static int[] result;
+
+    static int[] duplicated;
+    static int n = 0;
+
+    static long count = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        n = Integer.parseInt(st.nextToken());
+
+        board = new int[n+1];
+        result = new int[n+1];
+        duplicated = new int[n+1];
+
+        for(int i=1;i<=n;i++){
+            board[i] = Integer.parseInt(br.readLine());
+        }
+
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i=1;i<=n;i++){
+
+            int tmpCount = 0;
+
+            while(!stack.isEmpty()){
+                if(board[stack.peek()] < board[i]){
+                    count += duplicated[stack.peek()] + 1;
+                    tmpCount += duplicated[stack.peek()] + 1;
+                    stack.pop();
+                }else if(board[stack.peek()] == board[i]){
+                    duplicated[i] += duplicated[stack.peek()] + 1;
+                    count += duplicated[stack.peek()] + 1;
+                    stack.pop();
+                } else{
+                    count++;
+                    break;
+                }
+            }
+
+            stack.push(i);
+        }
+
+        System.out.println(count);
+
+        br.close();
+    }
+}
