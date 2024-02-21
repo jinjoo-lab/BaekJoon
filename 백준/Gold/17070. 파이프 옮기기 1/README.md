2b4420218cfbe21631b3809f068a4322058cdc83
진주원(JooWon Jin) <84346055+jinjoo-lab@users.noreply.github.com>
2024-02-21 15:30:14 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 252 ms, Memory: 12540 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/README.md
+++ 백준/Gold/17070. 파이프 옮기기 1/README.md
@@ -4,11 +4,15 @@
 
 ### 성능 요약
 
-메모리: 18912 KB, 시간: 276 ms
+메모리: 12540 KB, 시간: 252 ms
 
 ### 분류
 
-다이나믹 프로그래밍(dp), 그래프 이론(graphs), 그래프 탐색(graph_traversal)
+다이나믹 프로그래밍, 그래프 이론, 그래프 탐색
+
+### 제출 일자
+
+2024년 2월 21일 15:29:31
 
 ### 문제 설명
 
@@ -20,7 +24,7 @@
 
 <p>파이프는 회전시킬 수 있으며, 아래와 같이 3가지 방향이 가능하다.</p>
 
-<p style="text-align: center;"><img alt="" src="https://upload.acmicpc.net/b29efafa-dbae-4522-809c-76d5c184a231/-/preview/" style="width: 427px; height: 136px;"></p>
+<p style="text-align: center;"><img alt="" src="" style="width: 427px; height: 136px;"></p>
 
 <p>파이프는 매우 무겁기 때문에, 유현이는 파이프를 밀어서 이동시키려고 한다. 벽에는 새로운 벽지를 발랐기 때문에, 파이프가 벽을 긁으면 안 된다. 즉, 파이프는 항상 빈 칸만 차지해야 한다.</p>
 
@@ -30,15 +34,15 @@
 
 <p>아래 그림은 파이프가 놓여진 방향에 따라서 이동할 수 있는 방법을 모두 나타낸 것이고, 꼭 빈 칸이어야 하는 곳은 색으로 표시되어져 있다.</p>
 
-<p style="text-align: center;"><img alt="" src="https://upload.acmicpc.net/0f445b26-4e5b-4169-8a1a-89c9e115907e/-/preview/" style="width: 578px; height: 203px;"></p>
+<p style="text-align: center;"><img alt="" src="" style="width: 578px; height: 203px;"></p>
 
 <p style="text-align: center;">가로</p>
 
-<p style="text-align: center;"><img alt="" src="https://upload.acmicpc.net/045d071f-0ea2-4ab5-a8db-61c215e7e7b7/-/preview/" style="width: 579px; height: 203px;"></p>
+<p style="text-align: center;"><img alt="" src="" style="width: 579px; height: 203px;"></p>
 
 <p style="text-align: center;">세로</p>
 
-<p style="text-align: center;"><img alt="" src="https://upload.acmicpc.net/ace5e982-6a52-4982-b51d-6c33c6b742bf/-/preview/" style="width: 886px; height: 203px;"></p>
+<p style="text-align: center;"><img alt="" src="" style="width: 886px; height: 203px;"></p>
 
 <p style="text-align: center;">대각선</p>
 
--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -1,126 +1,131 @@
-import java.io.*;
-import java.util.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     static int n = 0;
-    static int[][] board = new int[18][18];
-    static int[] dx = {0, 1, 1};
-    static int[] dy = {1, 0, 1};
-    static int result = 0;
+
+    static int[] dx = {0,1,1};
+    static int[] dy = {1,0,1};
+
+    static int[][] board;
+
+    static int count = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        StringBuilder sb = new StringBuilder();
+
         n = Integer.parseInt(st.nextToken());
 
-        for (int i = 1; i <= n; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            for (int j = 1; j <= n; j++) {
+        board = new int[n+1][n+1];
+
+        for(int i = 1;  i <= n ; i++){
+            st = new StringTokenizer(br.readLine()," ");
+
+            for(int j = 1; j <= n ; j++){
                 board[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
         dfs(1,2,0);
-        System.out.println(result);
+        System.out.println(count);
+
+        br.close();
     }
-    static void dfs(int x,int y,int dir)
-    {
-        if(x==n&&y==n)
-        {
-            result++;
+
+    static void dfs(int x,int y,int dir){
+
+        if(x == n && y == n){
+            count++;
             return;
         }
 
-        if(dir==0)
-        {
-            int nx = x + dx[0];
-            int ny = y + dy[0];
+        int nx = 0;
+        int ny = 0;
 
-            if(nx>=1&&ny>=1&&nx<=n&&ny<=n)
-            {
-                if(board[nx][ny]==0)
-                {
+        if(dir == 0){
+            // g -> g
+            // g -> c
+            boolean can = true;
+
+            for(int i = 0 ; i < 3 ; i++){
+
+                nx = x + dx[i];
+                ny = y + dy[i];
+
+                if(nx < 1 || nx > n || ny < 1 || ny > n) {
+                    can = false;
+                    continue;
+                }
+
+                if(board[nx][ny] == 1) {
+                    can = false;
+                    continue;
+                }
+
+                if(i == 0){
                     dfs(nx,ny,0);
+                }else if(i == 2 && can){
+                    dfs(nx,ny,2);
                 }
             }
-            for(int i=0;i<3;i++) {
+        }else if(dir == 1){
+            // s -> s
+            // s -> c
+            boolean can = true;
+
+            for(int i = 0 ; i < 3 ; i++){
+
                 nx = x + dx[i];
                 ny = y + dy[i];
 
-
-                if (nx >= 1 && ny >= 1 && nx <= n && ny <= n) {
-                    if (board[nx][ny] == 1) {
-                        return;
-                    }
-                } else {
-                    return;
+                if(nx < 1 || nx > n || ny < 1 || ny > n) {
+                    can = false;
+                    continue;
                 }
-            }
-            dfs(nx, ny, 2);
-        }
-        else if(dir==1)
-        {
-            int nx = x + dx[1];
-            int ny = y + dy[1];
 
-            if(nx>=1&&ny>=1&&nx<=n&&ny<=n)
-            {
-                if(board[nx][ny]==0)
-                {
+                if(board[nx][ny] == 1) {
+                    can = false;
+                    continue;
+                }
+
+                if(i == 1){
                     dfs(nx,ny,1);
+                }else if(i == 2 && can){
+                    dfs(nx,ny,2);
                 }
             }
-            for(int i=0;i<3;i++) {
+        }else{
+            // s -> s
+            // s -> c
+            boolean can = true;
+
+            for(int i = 0 ; i < 3 ; i++){
+
                 nx = x + dx[i];
                 ny = y + dy[i];
 
+                if(nx < 1 || nx > n || ny < 1 || ny > n) {
+                    can = false;
+                    continue;
+                }
 
-                if (nx >= 1 && ny >= 1 && nx <= n && ny <= n) {
-                    if (board[nx][ny] == 1) {
-                        return;
-                    }
-                } else {
-                    return;
+                if(board[nx][ny] == 1) {
+                    can = false;
+                    continue;
+                }
+
+                if(i == 0 || i == 1){
+                    dfs(nx,ny,i);
+                }else if(i == 2 && can){
+                    dfs(nx,ny,2);
                 }
             }
-            dfs(nx, ny, 2);
-        }
-        else if(dir==2)
-        {
-            int nx = x + dx[0];
-            int ny = y + dy[0];
-
-            if(nx>=1&&ny>=1&&nx<=n&&ny<=n)
-            {
-                if(board[nx][ny]==0)
-                {
-                    dfs(nx,ny,0);
-                }
-            }
-            nx = x + dx[1];
-            ny = y + dy[1];
-
-            if(nx>=1&&ny>=1&&nx<=n&&ny<=n)
-            {
-                if(board[nx][ny]==0)
-                {
-                    dfs(nx,ny,1);
-                }
-            }
-            for(int i=0;i<3;i++) {
-                nx = x + dx[i];
-                ny = y + dy[i];
-
-
-                if (nx >= 1 && ny >= 1 && nx <= n && ny <= n) {
-                    if (board[nx][ny] == 1) {
-                        return;
-                    }
-                } else {
-                    return;
-                }
-            }
-            dfs(nx, ny, 2);
         }
     }
+
 }
