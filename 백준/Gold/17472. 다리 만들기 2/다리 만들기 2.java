81d49cfd8e5628dc9ad0d6bce7d5492ccd4d1006
진주원(JooWon Jin) <84346055+jinjoo-lab@users.noreply.github.com>
2024-02-23 15:52:45 +0900

    [Gold I] Title: 다리 만들기 2, Time: 132 ms, Memory: 14320 KB -BaekjoonHub

--- 백준/Gold/17472. 다리 만들기 2/다리 만들기 2.java
+++ 백준/Gold/17472. 다리 만들기 2/다리 만들기 2.java
@@ -1,127 +1,97 @@
-import java.io.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
-
-    static int n = 0;
-    static int m = 0;
+    static int[] dx = {0, 0, -1, 1};
+    static int[] dy = {1, -1, 0, 0};
 
     static int[][] board;
-    static int[][] land;
 
-    static int[] dx = {0,0,-1,1};
-    static int[] dy = {1,-1,0,0};
-
-    static Queue<Point>[] landPoint = new LinkedList[7];
-
-    static PriorityQueue<Point> pq = new PriorityQueue<>(
-            (x,y) -> x.d - y.d
-    );
+    static int[][] v;
+    static int n, m;
 
     static int[] root;
 
-    public static void main(String[] args) throws IOException {
+    static Queue<int[]>[] border = new ArrayDeque[7];
+
+    static PriorityQueue<int[]> pq = new PriorityQueue<>(
+            (x,y) -> x[2] - y[2]
+    );
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringBuilder sb = new StringBuilder();
 
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
 
         board = new int[n+1][m+1];
-        land = new int[n+1][m+1];
+        v = new int[n+1][m+1];
 
-        for(int i=1;i<=n;i++){
-            st = new StringTokenizer(br.readLine()," ");
+        for(int i= 1 ; i <= n ; i++){
+            st = new StringTokenizer(br.readLine(), " ");
 
-            for(int j=1;j<=m;j++){
+            for(int j= 1 ;j <=m ;j++){
                 board[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        for(int i=1;i<=6;i++){
-            landPoint[i] = new LinkedList<Point>();
+        int num = 1;
+
+        for(int i= 1  ; i <= 6; i++){
+            border[i] = new ArrayDeque<>();
         }
 
-        int num = 1;
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                if(board[i][j] == 1 && land[i][j] == 0){
-                    find(num,i,j);
-                    num = num + 1;
+        for(int i=1 ; i <= n ;i++){
+            for(int j=1 ;j<=m ;j++){
+                if(v[i][j] == 0 && board[i][j] == 1){
+                    bfs(i,j,num++);
                 }
             }
         }
 
-        root = new int[num+1];
-
-        num = num - 1;
-        for(int i=1;i<=num;i++){
+        root = new int[num];
+        for(int i= 1 ; i < num ; i++){
             root[i] = i;
-            dis(i);
+        }
+        num -= 1;
+
+        for(int i=1  ;i <= num ; i++){
+            makeDis(i);
         }
 
-        int count = 0;
         int result = 0;
-        while(!pq.isEmpty()){
-            Point cur = pq.poll();
+        int count = 0;
 
-            if(find(cur.x) != find(cur.y)){
+
+        while(!pq.isEmpty()){
+            int[] tmp = pq.poll();
+
+            if(find(tmp[0]) != find(tmp[1])){
+                union(tmp[0],tmp[1]);
                 count += 1;
-                result += cur.d;
-                union(cur.x,cur.y);
+                result += tmp[2];
             }
 
             if(count == num -1)
                 break;
         }
 
-        if(count == num -1){
-            System.out.println(result);
-        }
-
-        else{
+        if(count != num - 1)
             System.out.println(-1);
-        }
+        else
+            System.out.println(result);
 
         br.close();
     }
 
-    static void print(){
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                System.out.print(land[i][j]+" ");
-            }
-            System.out.println();
-        }
-        System.out.println();
-    }
-
-    static void find(int num,int x,int y){
-        Queue<Node> q = new LinkedList<>();
-        q.add(new Node(x,y));
-        land[x][y] = num;
-        landPoint[num].add(new Point(x,y,-1));
-
-        while(!q.isEmpty()){
-            Node cur = q.poll();
-
-            for(int i=0;i<4;i++){
-                int nx = cur.x + dx[i];
-                int ny = cur.y + dy[i];
-
-                if(nx < 1 || nx > n || ny < 1 || ny > m)
-                    continue;
-
-                if(land[nx][ny] == 0 && board[nx][ny] == 1){
-                    land[nx][ny] = num;
-                    q.add(new Node(nx,ny));
-                    landPoint[num].add(new Point(nx,ny,-1));
-                }
-            }
-        }
-    }
     static int find(int x){
-        if(root[x] == x)
+        if(x == root[x])
             return root[x];
 
         return root[x] = find(root[x]);
@@ -131,79 +101,102 @@ public class Main {
         x = find(x);
         y = find(y);
 
-        if(x < y){
+        if(x < y)
             root[y] = x;
-        }
-
-        else{
+        else
             root[x] = y;
-        }
     }
 
-    static void dis(int num){
-        int[][][] dis = new int[n+1][m+1][4];
-        while(!landPoint[num].isEmpty()){
-            Point cur = landPoint[num].poll();
+    static void makeDis(int num){
 
-            if(cur.d == -1){
-                for(int i =0;i < 4;i++){
-                    int nx = cur.x + dx[i];
-                    int ny = cur.y + dy[i];
+        int[] dis = new int[7];
+
+        while(!border[num].isEmpty()){
+            int[] cur = border[num].poll();
+
+            if(cur[2] == -1){
+                for(int i = 0 ; i < 4 ; i++){
+                    int nx = cur[0] + dx[i];
+                    int ny = cur[1] + dy[i];
 
                     if(nx < 1 || nx > n || ny < 1 || ny > m)
                         continue;
 
-                    if(land[nx][ny] != 0)
+                    if(v[nx][ny] == num)
                         continue;
 
-                    if(dis[nx][ny][i] == 0){
-                        dis[nx][ny][i] = 1;
-                        landPoint[num].add(new Point(nx,ny,i));
+                    if(board[nx][ny] == 0){
+                        border[num].add(new int[]{nx,ny,i,1});
                     }
                 }
             }
-
             else{
-                int nx = cur.x + dx[cur.d];
-                int ny = cur.y + dy[cur.d];
+                int nx = cur[0] + dx[cur[2]];
+                int ny = cur[1] + dy[cur[2]];
 
                 if(nx < 1 || nx > n || ny < 1 || ny > m)
                     continue;
 
-                if(land[nx][ny] == num)
+                if(v[nx][ny] == num)
                     continue;
 
-                if(dis[nx][ny][cur.d] == 0){
-                   if(land[nx][ny] == 0){
-                       dis[nx][ny][cur.d] = dis[cur.x][cur.y][cur.d] + 1;
-                       landPoint[num].add(new Point(nx,ny,cur.d));
-                   }
+                if(board[nx][ny] == 0){
+                    border[num].add(new int[]{nx,ny,cur[2],cur[3] + 1});
+                }else if(v[nx][ny] != num){
+                    if(cur[3] < 2)
+                        continue;
 
-                   else{
-                       if(dis[cur.x][cur.y][cur.d] >= 2){
-                           pq.add(new Point(num,land[nx][ny],dis[cur.x][cur.y][cur.d]));
-                       }
-                   }
+                    dis[v[nx][ny]] = (dis[v[nx][ny]] == 0 ) ? cur[3] : Math.min(dis[v[nx][ny]], cur[3]);
                 }
             }
         }
-    }
-}
-class Node{
-    int x;
-    int y;
 
-    Node(int x,int y){
-        this.x = x;
-        this.y = y;
-    }
-}
-class Point{
-    int x;int y;int d;
+        for(int i = 1 ; i <= 6 ; i++){
 
-    Point(int x,int y,int d){
-        this.x = x;
-        this.y = y;
-        this.d = d;
+            if(dis[i] < 2 || i == num)
+                continue;
+
+            pq.add(new int[]{num,i,dis[i]});
+        }
+
+    }
+
+    static void print(){
+        for(int i= 1 ; i<= n ;i++){
+            for(int j=1 ; j<=m ;j++){
+                System.out.print(v[i][j]+" ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    static void bfs(int x , int y,int num){
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{x,y});
+        v[x][y] = num;
+
+        while(!q.isEmpty()){
+            int[] cur = q.poll();
+
+            for(int i = 0 ; i < 4 ; i++){
+                int nx = cur[0] + dx[i];
+                int ny = cur[1] + dy[i];
+
+                if(nx < 1 || nx > n || ny < 1 || ny > m)
+                    continue;
+
+                if(v[nx][ny] != 0)
+                    continue;
+
+                if(board[nx][ny] == 0){
+                    border[num].add(new int[]{cur[0],cur[1],-1});
+                    continue;
+                }
+
+                v[nx][ny] = num;
+                q.add(new int[]{nx,ny});
+            }
+        }
     }
 }
