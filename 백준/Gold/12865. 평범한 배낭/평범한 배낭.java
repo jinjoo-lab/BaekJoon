24a1abb0c74d370001197f6a2d9f30c8dc199f66
진주원(JooWon Jin) <84346055+jinjoo-lab@users.noreply.github.com>
2024-02-28 11:29:18 +0900

    [Gold V] Title: 평범한 배낭, Time: 164 ms, Memory: 14712 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,49 +1,46 @@
-import java.util.*;
+
 import java.io.*;
+import java.util.*;
 
 public class Main {
-
     static int n = 0;
     static int m = 0;
-    static int h = 0;
+
+    static int[] dp = new int[100_001];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        StringTokenizer st = new StringTokenizer(br.readLine()," ");
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
 
-        int[][] bag = new int[n+1][2];
-        int[][] dp = new int[n+1][m+1];
+        int[][] board = new int[n+1][2];
 
+        for(int i = 1 ; i <= n ; i++){
+            st = new StringTokenizer(br.readLine(), " ");
 
-        for(int i=1;i<=n;i++)
-        {
-            st = new StringTokenizer(br.readLine()," ");
-
-            int w = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-            bag[i][0] = w;
-            bag[i][1] = v;
+            board[i][0] = Integer.parseInt(st.nextToken());
+            board[i][1] = Integer.parseInt(st.nextToken());
         }
-        int result = 0;
 
-        for(int i=1;i<=n;i++)
-        {
-            for(int j=0;j<=m;j++)
-            {
-                if(j < bag[i][0])
-                    dp[i][j] = dp[i-1][j];
+        int result  = 0;
 
-                else{
-                    dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j - bag[i][0]] + bag[i][1]);
-                    result = Math.max(result , dp[i][j]);
+        for(int i = 1 ; i <= n ; i++){
+            int w = board[i][0];
+            int h = board[i][1];
+
+            for(int j = m ; j >= 0 ; j --){
+                if(j - w >= 0){
+                    dp[j] = Math.max(dp[j] , dp[j - w] + h);
                 }
+
+                result = Math.max(result,dp[j]);
             }
         }
 
         System.out.println(result);
         br.close();
     }
+
 }
