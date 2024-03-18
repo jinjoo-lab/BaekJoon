fc0c1c87065b7d5c2d20e39678326f83f0118866
진주원(JooWon Jin) <84346055+jinjoo-lab@users.noreply.github.com>
2024-03-18 22:40:46 +0900

    [Gold II] Title: 등산, Time: 88 ms, Memory: 11752 KB -BaekjoonHub

--- 백준/Gold/1486. 등산/등산.java
+++ 백준/Gold/1486. 등산/등산.java
@@ -1,101 +1,191 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
-
-class Node {
-    int r, c, d;
-    public Node(int r, int c, int d) {
-        this.r = r;
-        this.c = c;
-        this.d = d;
-    }
-}
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    private static StringTokenizer st;
-    private static int r,c,t,d;
-    private static int[][] map;
-    private static int DIST_LIMIT = 52*52*25*25+1;
 
-    private static int solution(int[][] a, int[][] b) {
-        int max = 0;
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
-                if (a[i][j] + b[i][j] > d)  continue;
-                max = Math.max(max, map[i][j]);
-            }
-        }
-        return max;
-    }
+    static int n = 0;
+    static int m = 0;
+    static int k = 0;
+    static int d = 0;
 
-    private static int getGap(int a, int b) {
-        if (Math.abs(a-b) > t) return -1;
-        if (a >= b) return 1;
-        return (b-a) * (b-a);
-    }
+    static int[][] board;
 
-    private static int[][] f(boolean reverse) {
-        PriorityQueue<Node> pq = new PriorityQueue<>(new Comparator<Node>() {
-            @Override
-            public int compare(Node o1, Node o2) {
-                return o1.d - o2.d;
-            }
-        });
-        int[][] dist = new int[r][c];
-        boolean[][] v = new boolean[r][c];
+    static int[][] tmpDis;
 
-        for (int[] row : dist) Arrays.fill(row, DIST_LIMIT);
-        pq.add(new Node(0, 0, 0));
-        dist[0][0] = 0;
+    static int[] dx = {0,0,-1,1};
+    static int[] dy = {1,-1,0,0};
 
-        while (!pq.isEmpty()) {
-            Node cur = pq.poll();
-            int cr = cur.r;
-            int cc = cur.c;
-            int cd = cur.d;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        d = Integer.parseInt(st.nextToken());
 
-            if (v[cr][cc]) continue;
-            v[cr][cc] = true;
+        board = new int[n + 1][m + 1];
+        tmpDis = new int[n+1][m+1];
 
-            for (int dr = -1; dr <= 1; dr++) {
-                for (int dc = -1; dc <= 1; dc++) {
-                    if (((dr^dc)&1) != 1) continue;
-                    int nr = cr + dr;
-                    int nc = cc + dc;
-                    if (nr < 0 || nr >= r || nc < 0 || nc >= c) continue;
-                    int gap = !reverse ? getGap(map[cr][cc], map[nr][nc]) : getGap(map[nr][nc], map[cr][cc]);
-                    if (gap == -1 || dist[nr][nc] < cd+gap) continue;
+        for(int i = 0 ; i < n ; i++){
+            char[] arr = br.readLine().toCharArray();
 
-                    dist[nr][nc] = cd+gap;
-                    pq.add(new Node(nr, nc, dist[nr][nc]));
+            for(int j = 0 ; j < m ; j++){
+                if(arr[j] >= 'A' && arr[j] <= 'Z'){
+                    board[i][j] = arr[j] - 'A';
+                }
+
+                else{
+                    board[i][j] = arr[j] - 'a' + 26;
                 }
             }
         }
-        return dist;
+
+        bfs();
+        bfs2();
+
+        int result = board[0][0];
+
+        for(int i = 0 ; i <  n ; i++){
+            for(int j = 0; j < m ; j++){
+
+                if(tmpDis[i][j] <= d){
+                    result = Math.max(result,board[i][j]);
+                }
+            }
+        }
+
+        System.out.println(result);
+        br.close();
     }
 
-    private static int ni() { return Integer.parseInt(st.nextToken()); }
+    static void bfs(){
+        int result = 0;
+        Queue<int[]> q = new ArrayDeque<>();
+        int[][] dis = new int[n][m];
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < m ; j++){
+                dis[i][j] = d + 1;
+            }
+        }
 
-    private static void init() throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        st = new StringTokenizer(br.readLine());
-        r=ni(); c=ni(); t=ni(); d=ni();
-        map = new int[r][c];
-        for (int i = 0; i < r; i++) {
-            String row = br.readLine();
-            for (int j = 0; j < c; j++) {
-                char tmp = row.charAt(j);
-                map[i][j] = tmp >= 'a' ? tmp - 'a' + 26 : tmp - 'A';
+        dis[0][0] = 0;
+        q.add(new int[]{0,0,0});
+
+        while(!q.isEmpty()){
+            int[] cur = q.poll();
+
+            if(dis[cur[0]][cur[1]] < cur[2])
+                continue;
+
+            dis[cur[0]][cur[1]] = cur[2];
+
+            for(int i = 0 ; i < 4 ; i++){
+                int nx = cur[0] + dx[i];
+                int ny = cur[1] + dy[i];
+
+                if(nx < 0 || nx >= n || ny < 0 || ny >= m)
+                    continue;
+
+                if(Math.abs(board[nx][ny] - board[cur[0]][cur[1]]) > k)
+                    continue;
+
+                if(board[nx][ny] > board[cur[0]][cur[1]]){
+                    int tmpDis = Math.abs(board[nx][ny] - board[cur[0]][cur[1]]);
+                    tmpDis = tmpDis * tmpDis;
+
+                    int next = dis[cur[0]][cur[1]] + tmpDis;
+
+                    if(next > d)
+                        continue;
+
+                    if(dis[nx][ny] > next){
+                        dis[nx][ny] = next;
+                        q.add(new int[]{nx,ny,dis[nx][ny]});
+                    }
+
+                }else{
+
+                    if(dis[cur[0]][cur[1]] + 1 > d)
+                        continue;
+
+                    if(dis[nx][ny] > dis[cur[0]][cur[1]] + 1) {
+                        dis[nx][ny] = dis[cur[0]][cur[1]] + 1;
+                        q.add(new int[]{nx,ny,dis[nx][ny]});
+                    }
+                }
+            }
+        }
+
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < m ; j++){
+                tmpDis[i][j] += dis[i][j];
             }
         }
     }
 
-    public static void main(String[] args) throws Exception {
-        init();
-        int answer = solution(f(false), f(true));
-        System.out.println(answer);
+    static void bfs2(){
+        int result = 0;
+
+        Queue<int[]> q = new ArrayDeque<>();
+        int[][] dis = new int[n][m];
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < m ; j++){
+                dis[i][j] = d + 1;
+            }
+        }
+
+        dis[0][0] = 0;
+        q.add(new int[]{0,0,0});
+
+        while(!q.isEmpty()){
+            int[] cur = q.poll();
+
+            if(dis[cur[0]][cur[1]] < cur[2])
+                continue;
+
+            dis[cur[0]][cur[1]] = cur[2];
+
+            for(int i = 0 ; i < 4 ; i++){
+                int nx = cur[0] + dx[i];
+                int ny = cur[1] + dy[i];
+
+                if(nx < 0 || nx >= n || ny < 0 || ny >= m)
+                    continue;
+
+                if(Math.abs(board[nx][ny] - board[cur[0]][cur[1]]) > k)
+                    continue;
+
+                if(board[nx][ny] < board[cur[0]][cur[1]]){
+                    int tmpDis = Math.abs(board[nx][ny] - board[cur[0]][cur[1]]);
+                    tmpDis = tmpDis * tmpDis;
+
+                    int next = dis[cur[0]][cur[1]] + tmpDis;
+
+                    if(next > d)
+                        continue;
+
+                    if(dis[nx][ny] > next){
+                        dis[nx][ny] = next;
+                        q.add(new int[]{nx,ny,dis[nx][ny]});
+                    }
+
+                }else{
+
+                    if(dis[cur[0]][cur[1]] + 1 > d)
+                        continue;
+
+                    if(dis[nx][ny] > dis[cur[0]][cur[1]] + 1) {
+                        dis[nx][ny] = dis[cur[0]][cur[1]] + 1;
+                        q.add(new int[]{nx,ny,dis[nx][ny]});
+                    }
+                }
+            }
+        }
+
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < m ; j++){
+                tmpDis[i][j] += dis[i][j];
+            }
+        }
     }
 }
