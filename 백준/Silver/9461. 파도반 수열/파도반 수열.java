347016a497cd088c50daa471bcb6cce0cff7bc55
진주원(JooWon Jin) <84346055+jinjoo-lab@users.noreply.github.com>
2024-04-02 12:12:36 +0900

    [Silver III] Title: 파도반 수열, Time: 80 ms, Memory: 11608 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.java
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -1,27 +1,25 @@
 import java.io.*;
-import java.math.BigInteger;
 
 public class Main {
     static int n = 0;
-    static BigInteger[] dp = new BigInteger[101];
+    static long[] dp = new long[101];
     public static void main(String[] args) throws IOException {
-        dp[1]=new BigInteger("1");dp[2]=new BigInteger("1");
-        dp[3]=new BigInteger("1");dp[4]=new BigInteger("2");
-        dp[5]=new BigInteger("2");
+        dp[1] = 1; dp[2] = 1; dp[3] = 1;
+        dp[4] = 2; dp[5] = 2;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         n = Integer.parseInt(br.readLine());
         for(int k=1;k<=n;k++)
         {
             int num = Integer.parseInt(br.readLine());
-            BigInteger result = find(num);
+            long result = find(num);
             bw.write(result+"\n");
         }
         bw.flush();
         bw.close();
         br.close();
     }
-    static BigInteger find(int num)
+    static long find(int num)
     {
         if(num<=5)
         {
@@ -30,7 +28,7 @@ public class Main {
         else{
             for(int i=6;i<=num;i++)
             {
-                dp[i] = dp[i-1].add(dp[i-5]);
+                dp[i] = dp[i-1] + dp[i-5];
             }
             return dp[num];
         }
