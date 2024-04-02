0354238eef00c3f94224f927f6aa5499378886f7
진주원(JooWon Jin) <84346055+jinjoo-lab@users.noreply.github.com>
2024-04-02 12:43:56 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 332 ms, Memory: 22528 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -1,82 +1,91 @@
+
 import java.io.*;
-import java.util.*;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Main {
     static int n = 0;
     static int[][] board;
-    static int[][] dis;
-    static int[] dx = {1,-1,0,0};
-    static int[] dy = {0,0,1,-1};
 
-    static int MAX = 210000000;
+    static int[] dx = {0,0,-1,1};
+    static int[] dy = {1,-1,0,0};
+
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st ;
-        int count = 1;
-        while(true) {
-            st = new StringTokenizer(br.readLine()," ");
+        StringBuilder sb = new StringBuilder();
+
+        int idx = 1;
+
+        while(true){
+            StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
             n = Integer.parseInt(st.nextToken());
-            if(n==0)
+
+            if(n == 0)
                 break;
 
-            board = new int[126][126];
-            dis = new int[126][126];
+            board = new int[n+1][n+1];
 
-            for (int i = 1; i <= n; i++) {
-                st = new StringTokenizer(br.readLine(), " ");
-                for (int j = 1; j <= n; j++) {
+            for(int i = 1 ; i <= n ; i++){
+                st = new StringTokenizer(br.readLine()," ");
+                for(int j = 1 ; j <= n ; j++){
                     board[i][j] = Integer.parseInt(st.nextToken());
-                    dis[i][j] = MAX;
                 }
             }
 
-            PriorityQueue<point> q = new PriorityQueue<>(
-                    (o1, o2) -> o1.score - o2.score
-            );
+            int tmpResult = go();
 
-            bfs(q);
-
-            System.out.println("Problem "+count+": "+dis[n][n]);
-            count = count + 1;
+            sb.append("Problem "+idx+": "+tmpResult+"\n");
+            idx++;
         }
+
+        System.out.print(sb);
+
         br.close();
     }
 
-    static void bfs(PriorityQueue<point> q)
-    {
-        q.add(new point(1,1,board[1][1]));
+    static int go(){
+
+        int result = 0;
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>(
+                (x,y) -> x[2] - y[2]
+        );
+
+        pq.add(new int[]{1,1,board[1][1]});
+
+        int[][] dis = new int[n+1][n+1];
+        for(int i = 1 ; i <= n ; i++){
+            for(int j = 1 ; j<= n ; j++){
+                dis[i][j] = Integer.MAX_VALUE;
+            }
+        }
+
         dis[1][1] = board[1][1];
 
-        while(!q.isEmpty())
-        {
-            point cur = q.poll();
+        while(!pq.isEmpty()){
+            int[] cur = pq.poll();
 
-            for(int i=0;i<4;i++)
-            {
-                int nx = cur.x + dx[i];
-                int ny = cur.y + dy[i];
+            if(cur[0] == n && cur[1] == n){
+                result = cur[2];
+                break;
+            }
 
-                if(nx>=1&&nx<=n&&ny>=1&&ny<=n)
-                {
-                    if(dis[nx][ny]>dis[cur.x][cur.y] + board[nx][ny]) {
-                        dis[nx][ny] = Math.min(dis[nx][ny], dis[cur.x][cur.y] + board[nx][ny]);
-                        q.add(new point(nx,ny,dis[nx][ny]));
-                    }
+            for(int i = 0 ; i < 4 ; i++){
+                int nx = cur[0] + dx[i];
+                int ny = cur[1] + dy[i];
+
+                if(nx < 1 || nx > n || ny < 1 || ny > n)
+                    continue;
+
+                if(dis[nx][ny] > dis[cur[0]][cur[1]] + board[nx][ny]){
+                    dis[nx][ny] = dis[cur[0]][cur[1]] + board[nx][ny];
+                    pq.add(new int[]{nx,ny,dis[nx][ny]});
                 }
             }
         }
-    }
-}
-class point
-{
-    int x;
-    int y;
-    int score;
 
-    point(int x,int y,int score)
-    {
-        this.x = x;
-        this.y = y;
-        this.score = score;
+        return result;
     }
 }
