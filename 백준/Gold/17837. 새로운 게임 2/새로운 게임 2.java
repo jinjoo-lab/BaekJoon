0e7669f1cd19d3220e61583efefdd20de274a1f3
진주원(JooWon Jin) <84346055+jinjoo-lab@users.noreply.github.com>
2024-04-03 14:44:06 +0900

    [Gold II] Title: 새로운 게임 2, Time: 120 ms, Memory: 14340 KB -BaekjoonHub

--- 백준/Gold/17837. 새로운 게임 2/새로운 게임 2.java
+++ 백준/Gold/17837. 새로운 게임 2/새로운 게임 2.java
@@ -1,236 +1,217 @@
+
 import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int n = 0;
-    static int m = 0;
+
+    static int n,k;
     static int[][] board;
-    static Node[] where;
-    static ArrayList<Integer>[][] map;
 
-    static int[] dx = {0,0,0,-1,1};
-    static int[] dy = {0,1,-1,0,0};
+    static Point[] chess;
 
-    public static void main(String[] args) throws IOException {
+    static ArrayList<Integer>[][] cBoard;
+    static int dx[] = {0,0,0,-1,1};
+    static int dy[] = {0,1,-1,0,0};
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
 
         n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
 
         board = new int[n+1][n+1];
-        map = new ArrayList[n+1][n+1];
-        where = new Node[m+1];
+        cBoard = new ArrayList[n+1][n+1];
 
-        for(int i=1;i<=n;i++)
-        {
-            st = new StringTokenizer(br.readLine(), " ");
-            for(int j=1;j<=n;j++)
-            {
+        for(int i = 1 ; i <= n ; i ++){
+            st = new StringTokenizer(br.readLine()," ");
+
+            for(int j = 1 ; j <= n ; j++){
                 board[i][j] = Integer.parseInt(st.nextToken());
-                map[i][j] = new ArrayList<>();
+
+                cBoard[i][j] = new ArrayList<>();
             }
         }
 
-        for(int i=1;i<=m;i++)
-        {
-            st = new StringTokenizer(br.readLine(), " ");
-            int x= Integer.parseInt(st.nextToken());
-            int y= Integer.parseInt(st.nextToken());
-            int d= Integer.parseInt(st.nextToken());
+        chess = new Point[k+1];
 
-            where[i] = new Node(x,y,d);
-            map[x][y].add(i);
+        for(int i = 1 ; i <= k ; i++){
+            st = new StringTokenizer(br.readLine()," ");
+
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            int d = Integer.parseInt(st.nextToken());
+
+            chess[i] = new Point(x,y,d);
+            cBoard[x][y].add(i);
         }
 
-        int result = -1;
-        for(int i=1;i<=1000;i++)
-        {
-            boolean tmp = turn();
+        int result = 0;
 
-            if(!tmp){
-                result = i;
+        while(true) {
+            result += 1;
+
+            if(move())
                 break;
-            }
+
+            if(result > 1000)
+                break;
+
+        }
+
+        if(result > 1000) {
+            result = -1;
         }
 
         System.out.println(result);
+
         br.close();
     }
-    static boolean turn(){
 
-        for(int i=1;i<=m;i++)
-        {
-            boolean tmp = true;
-            Node cur = where[i];
+    static boolean isOut(int nx,int ny){
+        if(nx < 1 || nx > n || ny < 1 || ny > n)
+            return true;
 
-            int nx = cur.x + dx[cur.d];
-            int ny = cur.y + dy[cur.d];
-
-            if(nx < 1 || nx > n || ny < 1 || ny > n)
-            {
-                tmp = moveB(cur.x,cur.y,i);
-            }
-
-            else if(board[nx][ny] == 0)
-            {
-               tmp =  moveW(cur.x,cur.y,nx,ny,i);
-            }
-
-            else if(board[nx][ny] == 1){
-               tmp =  moveR(cur.x,cur.y,nx,ny,i);
-            }
-
-            else if(board[nx][ny] == 2){
-               tmp =  moveB(cur.x,cur.y,i);
-            }
-
-            if(!tmp)
-                return false;
-        }
-
-        return true;
-    }
-
-    static int changeD(int d)
-    {
-        if(d == 1)
-            return 2;
-        if(d == 2)
-            return 1;
-        if(d == 4)
-            return 3;
-
-        return 4;
-    }
-
-    static boolean moveR(int x,int y,int nx,int ny,int num)
-    {
-        ArrayList<Integer> tmp = new ArrayList<>();
-        Stack<Integer> q = new Stack<>();
-
-        boolean start = false;
-        for(int i=0;i<map[x][y].size();i++)
-        {
-            int cur = map[x][y].get(i);
-
-            if(cur == num)
-            {
-                start = true;
-            }
-
-            if(start)
-            {
-                q.add(cur);
-                where[cur] = new Node(nx,ny,where[cur].d);
-            }
-
-            else{
-                tmp.add(cur);
-            }
-        }
-
-        while(!q.isEmpty())
-        {
-            map[nx][ny].add(q.pop());
-        }
-
-        map[x][y] = tmp;
-
-        if(map[nx][ny].size() >= 4)
-            return false;
-
-        return true;
-    }
-
-    static boolean moveW(int x,int y,int nx,int ny,int num)
-    {
-        ArrayList<Integer> tmp = new ArrayList<>();
-        boolean start = false;
-        for(int i=0;i<map[x][y].size();i++)
-        {
-            int cur = map[x][y].get(i);
-
-            if(cur == num)
-            {
-                start = true;
-            }
-
-            if(start)
-            {
-                map[nx][ny].add(cur);
-                where[cur] = new Node(nx,ny,where[cur].d);
-            }
-
-            else{
-                tmp.add(cur);
-            }
-        }
-
-        map[x][y] = tmp;
-
-        if(map[nx][ny].size() >= 4)
-            return false;
-
-        return true;
-    }
-
-    static boolean moveB(int x,int y,int num){
-        int cd = changeD(where[num].d);
-        int n2x = x + dx[cd];
-        int n2y = y + dy[cd];
-
-        if(n2x < 1 || n2x > n || n2y < 1 || n2y > n)
-        {
-            where[num] = new Node(where[num].x,where[num].y,cd);
-        }
-
-        else if(board[n2x][n2y] == 2)
-        {
-            where[num] = new Node(where[num].x,where[num].y,cd);
-        }
-
-        else{
-            where[num] = new Node(where[num].x,where[num].y,cd);
-
-            if(board[n2x][n2y] == 0)
-            {
-                boolean tmp = moveW(x,y,n2x,n2y,num);
-                return tmp;
-            }
-
-            else if(board[n2x][n2y] == 1)
-            {
-                boolean tmp = moveR(x,y,n2x,n2y,num);
-                return tmp;
-            }
-        }
-
-        return true;
+        return false;
     }
 
     static void print(){
-        for(int i=1;i<=n;i++)
-        {
-            for(int j=1;j<=n;j++)
-            {
-                System.out.print(map[i][j].size()+" ");
+        for(int i =1 ; i <= n ;i++){
+            for(int j =1 ; j <= n ;j++){
+                System.out.print(cBoard[i][j].size()+" ");
             }
             System.out.println();
         }
         System.out.println();
     }
-}
-class Node
-{
-    int x;
-    int y;
-    int d;
 
-    Node(int x,int y,int d)
-    {
-        this.x = x;
-        this.y = y;
-        this.d = d;
+    static boolean move(){
+        for(int i = 1 ; i <= k ; i++){
+            int nx = chess[i].x + dx[chess[i].d];
+            int ny = chess[i].y + dy[chess[i].d];
+
+            if(isOut(nx,ny) || board[nx][ny] == 2){
+                int nd = changeDir(chess[i].d);
+
+                int n2x = chess[i].x + dx[nd];
+                int n2y = chess[i].y + dy[nd];
+
+                if(isOut(n2x,n2y) || board[n2x][n2y] == 2){
+                    chess[i].d = nd; // 방향만 바꾸기
+
+                    if(isOver(chess[i].x,chess[i].y))
+                        return true;
+                }else{
+                    chess[i].d = nd;
+
+                    if(board[n2x][n2y] == 0){
+                        if(moveW(i,chess[i].x,chess[i].y,n2x,n2y))
+                            return true;
+                    }else if(board[n2x][n2y] == 1){
+                        if(moveR(i,chess[i].x,chess[i].y,n2x,n2y))
+                            return true;
+                    }
+                }
+            } else if(board[nx][ny] == 0){
+                if(moveW(i,chess[i].x,chess[i].y,nx,ny))
+                    return true;
+
+            } else if(board[nx][ny] == 1) {
+                if(moveR(i,chess[i].x,chess[i].y,nx,ny))
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    static boolean isOver(int x,int y){
+        if(cBoard[x][y].size() >=4)
+            return true;
+
+        return false;
+    }
+
+    static boolean moveR(int idx,int x,int y,int nx,int ny) {
+        ArrayList<Integer> curList = new ArrayList<>();
+        Stack<Integer> tmpList = new Stack<>();
+
+        boolean start = false;
+
+        for(int i = 0 , size = cBoard[x][y].size(); i < size ; i++){
+
+            int num = cBoard[x][y].get(i);
+
+            if(num == idx) {
+                start = true;
+            }
+
+            if(start){
+                chess[num].x = nx;
+                chess[num].y = ny;
+                tmpList.add(num);
+            }else{
+                curList.add(num);
+            }
+        }
+
+        cBoard[x][y] = curList;
+
+        while(!tmpList.isEmpty()){
+            cBoard[nx][ny].add(tmpList.pop());
+        }
+
+        return cBoard[x][y].size() >= 4 || cBoard[nx][ny].size() >= 4;
+    }
+
+    static boolean moveW(int idx,int x,int y,int nx,int ny) {
+        ArrayList<Integer> curList = new ArrayList<>();
+        ArrayList<Integer> tmpList = new ArrayList<>();
+
+        boolean start = false;
+
+        for(int i = 0 , size = cBoard[x][y].size(); i < size ; i++){
+
+            int num = cBoard[x][y].get(i);
+
+            if(num == idx) {
+                start = true;
+            }
+
+            if(start){
+                chess[num].x = nx;
+                chess[num].y = ny;
+                tmpList.add(num);
+            }else{
+                curList.add(num);
+            }
+        }
+
+        cBoard[x][y] = curList;
+        cBoard[nx][ny].addAll(tmpList);
+
+        return cBoard[x][y].size() >= 4 || cBoard[nx][ny].size() >= 4;
+    }
+
+    static int changeDir(int cur){
+        if(cur == 1)
+            return 2;
+        if(cur == 2)
+            return 1;
+        if(cur == 3)
+            return 4;
+
+        return 3;
+    }
+
+    static class Point {
+        int x;
+        int y;
+        int d;
+
+        Point(int x,int y,int d){
+            this.x = x;
+            this.y = y;
+            this.d = d;
+        }
     }
 }
