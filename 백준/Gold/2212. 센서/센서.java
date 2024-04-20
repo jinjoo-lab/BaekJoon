45a7dc3f352b0d82b09e100e0f90d4fea6f83644
진주원(JooWon Jin) <84346055+jinjoo-lab@users.noreply.github.com>
2024-04-20 20:27:28 +0900

    [Gold V] Title: 센서, Time: 320 ms, Memory: 20108 KB -BaekjoonHub

--- 백준/Gold/2212. 센서/센서.java
+++ 백준/Gold/2212. 센서/센서.java
@@ -1,22 +1,23 @@
-import java.io.*;
+
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    static int n = 0;
-    static int m = 0;
+    static int n,k;
+    static int[] board;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
 
         n = Integer.parseInt(st.nextToken());
+        board = new int[n];
 
-        m = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine()," ");
+        k = Integer.parseInt(st.nextToken());
 
-        int[] board = new int[n];
-        st = new StringTokenizer(br.readLine(), " ");
-
-        for(int i=0;i<n;i++){
+        st = new StringTokenizer(br.readLine()," ");
+        for(int i = 0 ; i < n ; i++){
             board[i] = Integer.parseInt(st.nextToken());
         }
 
@@ -25,11 +26,11 @@ public class Main {
                 (x,y) -> y - x
         );
 
-        for(int i=1;i<n;i++){
+        for(int i = 1  ; i < n ; i++){
             pq.add(board[i] - board[i-1]);
         }
 
-        if(m >= n){
+        if(k >= n){
             System.out.println(0);
             return;
         }
@@ -39,7 +40,7 @@ public class Main {
         while(!pq.isEmpty()){
             int cur = pq.poll();
 
-            if(idx < m - 1) {
+            if(idx < k - 1) {
                 idx = idx + 1;
                 continue;
             }
