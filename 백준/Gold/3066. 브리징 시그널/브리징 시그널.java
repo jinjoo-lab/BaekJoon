fcab31b23f55e8e8883dd042d0f6666ea75cb27d
진주원(JooWon Jin) <84346055+jinjoo-lab@users.noreply.github.com>
2024-04-29 14:41:02 +0900

    [Gold II] Title: 브리징 시그널, Time: 340 ms, Memory: 33736 KB -BaekjoonHub

--- 백준/Gold/3066. 브리징 시그널/브리징 시그널.java
+++ 백준/Gold/3066. 브리징 시그널/브리징 시그널.java
@@ -2,43 +2,36 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-
     static int t,n;
-    static int[][] board;
-
-    static int[] dp;
-
+    static int[] input,dp;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine()," ");
         StringBuilder sb = new StringBuilder();
         t = Integer.parseInt(st.nextToken());
 
-        for(int a = 1 ; a <= t ; a++){
+        for(int tk = 1 ; tk <= t ; tk++){
             st = new StringTokenizer(br.readLine()," ");
             n = Integer.parseInt(st.nextToken());
 
-            board = new int[n+1][2];
+            input = new int[n+1];
             dp = new int[n+1];
 
             for(int i = 1 ; i <= n ; i++){
-                int tmp = Integer.parseInt(br.readLine());
-
-                board[i][0] = i;
-                board[i][1] = tmp;
+                input[i] = Integer.parseInt(br.readLine());
             }
 
-            dp[1] = board[1][1];
+            dp[1] = input[1];
             int i = 2;
-            int j=  1;
+            int j = 1;
 
             while(i <= n){
-                if(board[i][1] > dp[j]){
-                    dp[j+1] = board[i][1];
+                if(input[i] > dp[j]){
+                    dp[j+1] = input[i];
                     j++;
                 }else{
-                    int idx = bs(1,j,board[i][1]);
-                    dp[idx] = board[i][1];
+                    int idx = binarySearch(1,j,input[i]);
+                    dp[idx] = input[i];
                 }
 
                 i++;
@@ -47,30 +40,20 @@ public class Main {
             sb.append(j+"\n");
         }
         System.out.print(sb);
-
         br.close();
     }
-
-    static void print(){
-        for(int i = 1 ; i <= n ; i++){
-            System.out.print(dp[i]+" ");
-        }
-        System.out.println();
-    }
-
-    static int bs(int l,int r,int target){
+    static int binarySearch(int left,int right,int target){
         int mid = 0;
 
-        while(l < r){
-            mid = (l + r) / 2;
+        while(left <= right){
+            mid = (left + right) / 2;
 
             if(dp[mid] > target){
-                r = mid;
+                right = mid - 1;
             }else{
-                l = mid + 1;
+                left = mid + 1;
             }
         }
-
-        return r;
+        return left;
     }
 }
