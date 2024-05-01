852e86250e046e48ec92f25164f1376bd6427216
진주원(JooWon Jin) <84346055+jinjoo-lab@users.noreply.github.com>
2024-05-01 13:40:49 +0900

    [Gold V] Title: 내일 할거야, Time: 2536 ms, Memory: 260612 KB -BaekjoonHub

--- 백준/Gold/7983. 내일 할거야/내일 할거야.java
+++ 백준/Gold/7983. 내일 할거야/내일 할거야.java
@@ -2,49 +2,43 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int n = 0;
-    static int m = 0;
 
-    public static void main(String[] args) throws IOException {
+    static int n;
+    static int[][] board;
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
         n = Integer.parseInt(st.nextToken());
+        board = new int[n][2];
 
-        int[][] homework = new int[n][2];
+        for(int i = 0 ; i < n ; i++){
+            st = new StringTokenizer(br.readLine()," ");
 
-        for(int i=0;i<n;i++)
-        {
-            st = new StringTokenizer(br.readLine(), " ");
-            int t = Integer.parseInt(st.nextToken());
             int d = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
 
-            homework[i][0] = t;
-            homework[i][1] = d;
+            board[i][0] = d;
+            board[i][1] = e;
         }
 
-        Arrays.sort(homework,(x,y) ->{
-            if(x[1] == y[1])
+        Arrays.sort(board,(x,y) -> {
+            if(x[1] == y[1]){
                 return y[0] - x[0];
-
+            }
             return y[1] - x[1];
         });
 
-        int ld = homework[0][1];
-        int sd = homework[0][1] - homework[0][0];
+        int day = 1000_000_001;
 
-        for(int i=1;i<n;i++)
-        {
-            if(sd > homework[i][1])
-            {
-                sd = homework[i][1];
+        for(int i = 0 ;  i  < n  ; i++){
+            if(day > board[i][1]){
+                day = board[i][1];
             }
-
-            sd = sd - homework[i][0];
-
+            day = day - board[i][0];
         }
 
-        System.out.println(sd);
+        System.out.println(day);
 
         br.close();
     }
