0d6b047f62e8112cb4f4fcd35bb39d2114757884
진주원(JooWon Jin) <84346055+jinjoo-lab@users.noreply.github.com>
2024-05-06 01:01:40 +0900

    [Gold II] Title: 지각하면 안 돼, Time: 696 ms, Memory: 35228 KB -BaekjoonHub

--- 백준/Gold/12763. 지각하면 안 돼/지각하면 안 돼.java
+++ 백준/Gold/12763. 지각하면 안 돼/지각하면 안 돼.java
@@ -3,121 +3,101 @@ import java.io.*;
 
 public class Main {
 
-    static int n = 0;
-    static int t = 0;
-    static int m = 0;
+    static int n,t,m,l;
 
-    static int[][] dp;
-    static int k = 0;
-    static ArrayList<Node>[] graph;
-
-    static int result = -1;
+    static ArrayList<Load>[] arr;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
 
         n = Integer.parseInt(st.nextToken());
 
-        st = new StringTokenizer(br.readLine(), " ");
-
+        st = new StringTokenizer(br.readLine()," ");
         t = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
 
-        graph = new ArrayList[n+1];
+        st = new StringTokenizer(br.readLine()," ");
+        l = Integer.parseInt(st.nextToken());
+
+        arr = new ArrayList[n+1];
         for(int i = 1 ; i <= n ; i++){
-            graph[i] = new ArrayList<>();
+            arr[i] = new ArrayList<>();
         }
 
-        st = new StringTokenizer(br.readLine(), " ");
-
-        k = Integer.parseInt(st.nextToken());
-
-        for(int i = 1 ; i <= k ; i++){
-            st = new StringTokenizer(br.readLine(), " ");
-
+        for(int i = 1 ; i <= l ; i++){
+            st = new StringTokenizer(br.readLine()," ");
             int v1 = Integer.parseInt(st.nextToken());
             int v2 = Integer.parseInt(st.nextToken());
-            int t = Integer.parseInt(st.nextToken());
-            int m = Integer.parseInt(st.nextToken());
+            int tt = Integer.parseInt(st.nextToken());
+            int cc = Integer.parseInt(st.nextToken());
 
-            graph[v1].add(new Node(v2,t,m));
-            graph[v2].add(new Node(v1,t,m));
+            arr[v1].add(new Load(v2,tt,cc));
+            arr[v2].add(new Load(v1,tt,cc));
         }
 
-        search();
+        int returnV = start();
+        System.out.println(returnV);
 
-        System.out.println(result);
+
         br.close();
     }
 
-    static void search() {
+    static int start(){
 
-        int INF = Integer.MAX_VALUE;
+        int returnV = -1;
 
-        PriorityQueue<Node> pq = new PriorityQueue<>(
-                (x,y) -> x.t - y.t
+        PriorityQueue<Load> pq = new PriorityQueue<>(
+                (x,y) -> x.c - y.c
         );
 
-        dp = new int[n+1][10001];
-
-        pq.add(new Node(1,0,0));
-
-        for(int i = 2 ; i <= n ; i++){
-            for(int j = 0 ; j <= 10000 ; j ++)
-                dp[i][j] = INF;
+        int[][] d = new int[n+1][t+1];
+        for(int i = 1 ; i <= n ; i++){
+            for(int j = 0 ; j <= t ; j++){
+                d[i][j] = Integer.MAX_VALUE;
+            }
         }
 
-        while(!pq.isEmpty()) {
-            Node cur = pq.poll();
+        d[1][0] = 0;
+        pq.add(new Load(1,0,0));
 
-            if(cur.v == n){
-                if(result == -1){
-                    result = cur.m;
-                }else{
-                    result = Math.min(result,cur.m);
-                }
-                continue;
+        while(!pq.isEmpty()){
+            Load curP = pq.poll();
+
+            if(curP.v == n){
+                returnV = curP.c;
+                break;
             }
 
-            if(dp[cur.v][cur.t] < cur.m)
+            if(d[curP.v][curP.t] < curP.c)
                 continue;
 
-            for(Node next : graph[cur.v]) {
+            for(Load nextP : arr[curP.v]){
+                int nextC = curP.c + nextP.c;
+                int nextT = curP.t + nextP.t;
 
-                int nextT = cur.t + next.t;
-                int nextM = cur.m + next.m;
-
-                if(nextT > t || nextM > m)
+                if(nextC > m || nextT > t)
                     continue;
 
-
-                if(dp[next.v][nextT] >  nextM){
-                    dp[next.v][nextT] =  nextM;
-                    pq.add(new Node(next.v,nextT,nextM));
-
-                    for(int i = nextT + 1 ; i <= t; i++){
-                        if(dp[next.v][i] > dp[cur.v][cur.t] + nextM){
-                            dp[next.v][i] = dp[cur.v][cur.t] + nextM;
-                        }else{
-                            break;
-                        }
-                    }
+                if(d[nextP.v][nextT] > nextC){
+                    d[nextP.v][nextT] = nextC;
+                    pq.add(new Load(nextP.v,nextT,nextC));
                 }
             }
         }
 
+        return returnV;
     }
 
-    static class Node{
+    static class Load{
         int v;
         int t;
-        int m;
+        int c;
 
-        Node(int v,int t,int m){
+        Load(int v,int t,int c){
             this.v = v;
             this.t = t;
-            this.m = m;
+            this.c = c;
         }
     }
 }
