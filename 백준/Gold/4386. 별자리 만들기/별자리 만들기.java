9f370aba08d9102e0ac443bf894f2d868f000b2b
진주원(JooWon Jin) <84346055+jinjoo-lab@users.noreply.github.com>
2024-05-15 18:48:55 +0900

    [Gold III] Title: 별자리 만들기, Time: 236 ms, Memory: 18176 KB -BaekjoonHub

--- 백준/Gold/4386. 별자리 만들기/별자리 만들기.java
+++ 백준/Gold/4386. 별자리 만들기/별자리 만들기.java
@@ -1,115 +1,93 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    static int n = 0;
-    static int[] root;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
+    static int n;
+
+    static int[] root;
+    static double[][] board;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
         n = Integer.parseInt(st.nextToken());
+
+        board = new double[n+1][2];
         root = new int[n+1];
-        for(int i=1;i<=n;i++)
-        {
+        for(int i = 1 ; i <= n ; i++){
             root[i] = i;
         }
 
-        star[] arr = new star[n+1];
-
-        for(int i=1;i<=n;i++)
-        {
-            st = new StringTokenizer(br.readLine(), " ");
+        for(int i = 1 ; i <= n ; i++){
+            st = new StringTokenizer(br.readLine()," ");
             double x = Double.parseDouble(st.nextToken());
-            double y =  Double.parseDouble(st.nextToken());
-            star cur = new star(x,y);
+            double y = Double.parseDouble(st.nextToken());
 
-            arr[i] = cur;
+            board[i][0] = x;
+            board[i][1] = y;
         }
 
-        PriorityQueue<Edge> pq = new PriorityQueue<>(
-                (x,y) ->  Double.compare(x.cost , y.cost)
+        PriorityQueue<Node> pq = new PriorityQueue<>(
+                (x,y) -> Double.compare(x.dis,y.dis)
         );
 
-        for(int i=1;i<n;i++)
-        {
-            for(int j= i+1;j<=n;j++)
-            {
-                Edge edge = new Edge(i,j,cal(arr[i], arr[j]));
-
-                pq.add(edge);
+        for(int i = 1 ; i < n ; i++){
+            for(int j = i + 1 ; j <= n ; j++){
+                pq.add(new Node(i,j,calDis(i,j)));
             }
         }
 
-        double re = 0.0;
+        int count = 0;
+        double result = 0;
+
         while(!pq.isEmpty()) {
-            Edge cur = pq.poll();
+            Node cur = pq.poll();
 
-            if (find(cur.x) != find(cur.y))
-            {
-                re += cur.cost;
-                union(cur.x ,cur.y);
+            if(find(cur.v1) != find(cur.v2)){
+                union(cur.v1,cur.v2);
+                count++;
+                result += cur.dis;
             }
+
+            if(count == n - 1)
+                break;
         }
 
-        System.out.printf("%.2f",re);
-        System.out.println();
+        System.out.println(result);
         br.close();
     }
 
+    static double calDis(int idx1,int idx2){
+        return Math.sqrt(Math.pow(Math.abs(board[idx1][0] - board[idx2][0]),2) + Math.pow(Math.abs(board[idx1][1] - board[idx2][1]),2));
+    }
 
+    static class Node{
+        int v1;
+        int v2;
+        double dis;
 
-    static int find(int x)
-    {
-        if(x == root[x])
-            return x;
-        else{
-            return root[x] = find(root[x]);
+        Node(int v1,int v2,double dis){
+            this.v1 = v1;
+            this.v2 = v2;
+            this.dis = dis;
         }
     }
 
-    static void union(int x,int y)
-    {
+    static int find(int x){
+        if(x == root[x])
+            return root[x];
+
+        return root[x] = find(root[x]);
+    }
+
+    static void union(int x,int y){
         x = find(x);
         y = find(y);
 
         if(x < y)
             root[y] = x;
-
         else
             root[x] = y;
-    }
-
-    static double cal(star cur1 , star cur2)
-    {
-        double tmp1 = Math.pow((cur1.x - cur2.x),2);
-        double tmp2 = Math.pow((cur1.y - cur2.y),2);
-
-        return Math.sqrt(Math.abs(tmp2 + tmp1));
-    }
-}
-class star
-{
-    double x;
-    double y;
-
-    star(double x,double y)
-    {
-        this.x = x;
-        this.y = y;
-    }
-}
-
-class Edge
-{
-    int x;
-    int y;
-    double cost;
-
-    Edge(int x,int y, double cost)
-    {
-        this.x = x;
-        this.y = y;
-        this.cost = cost;
     }
 }
