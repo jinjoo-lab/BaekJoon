118b846b82c00d90741b4fc7d6c76b0bce2f4c80
진주원(JooWon Jin) <84346055+jinjoo-lab@users.noreply.github.com>
2024-07-12 00:08:40 +0900

    [Gold III] Title: 같이 눈사람 만들래?, Time: 868 ms, Memory: 12104 KB -BaekjoonHub

--- 백준/Gold/20366. 같이 눈사람 만들래？/같이 눈사람 만들래？.java
+++ 백준/Gold/20366. 같이 눈사람 만들래？/같이 눈사람 만들래？.java
@@ -2,28 +2,27 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-
+    static int result = Integer.MAX_VALUE;
     static int n;
     static int[] board;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine()," ");
-        n = Integer.parseInt(st.nextToken());
+
+        n = Integer.parseInt(br.readLine());
 
         board = new int[n];
-        st = new StringTokenizer(br.readLine()," ");
-        for(int i = 0 ; i < n ; i++){
+
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        for(int i = 0 ; i < n ; i++) {
             board[i] = Integer.parseInt(st.nextToken());
         }
 
         Arrays.sort(board);
 
-        for(int i = 0 ; i < n - 1 ; i++){
-            for(int j = i+1; j < n ; j++){
-                int tmpSum = board[i] + board[j];
-
-                find(i,j,tmpSum);
+        for(int i = 0 ; i < n - 1 ; i++) {
+            for(int j = i + 1 ; j < n ; j++) {
+                search(i,j);
             }
         }
 
@@ -32,45 +31,35 @@ public class Main {
         br.close();
     }
 
-    static int result = Integer.MAX_VALUE;
-    static void find(int tmpX,int tmpY,int tmpSum){
-
+    static void search(int idx1,int idx2) {
         int l = 0;
         int r = n - 1;
+        int target = board[idx1] + board[idx2];
 
-        while(l < r){
-            while(l == tmpX || l == tmpY){
-                l++;
-            }
+        while(l < r) {
+            while(l == idx1 || l == idx2)
+                l+= 1;
 
-            while(r == tmpY || r == tmpX){
-                r--;
-            }
+            while(r == idx2 || r == idx1)
+                r-= 1;
 
-            if(l >= r){
+            if(l >= r)
                 break;
-            }
 
-            int tmp = board[l] + board[r];
+            int tmpV = board[l] + board[r];
 
-            if(tmp == tmpSum){
+            result = Math.min(result, Math.abs(tmpV - target));
+
+            if(tmpV == target) {
                 result = 0;
-                break;
-            }else if(tmp > tmpSum){
-                result = Math.min(result, Math.abs(tmp - tmpSum));
-                r--;
-            }else{
-                result = Math.min(result, Math.abs(tmp - tmpSum));
-                l++;
+                return;
+            }
+
+            if(tmpV > target) {
+                r = r - 1;
+            }else {
+                l = l + 1;
             }
         }
-    }
-
-
-    static void print(){
-        for(int i = 0 ; i <  n ; i++){
-            System.out.print(board[i]+" ");
-        }
-        System.out.println();
     }
 }
