e1797e9afc4fcb000251d668a03fe56ceccaa2e2
진주원(JooWon Jin) <84346055+jinjoo-lab@users.noreply.github.com>
2024-09-05 12:37:52 +0900

    [Gold II] Title: 동전 분배, Time: 1492 ms, Memory: 14860 KB -BaekjoonHub

--- 백준/Gold/1943. 동전 분배/동전 분배.java
+++ 백준/Gold/1943. 동전 분배/동전 분배.java
@@ -5,6 +5,8 @@ public class Main {
 
     static int n;
     static boolean[] dp = new boolean[50_001];
+    static int[] coin;
+    static int[] num;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -18,6 +20,9 @@ public class Main {
             n = Integer.parseInt(br.readLine());
             dp[0] = true;
 
+            coin = new int[n+1];
+            num = new int[n+1];
+
             int sum = 0;
 
             for (int i = 1; i <= n; i++) {
@@ -26,10 +31,16 @@ public class Main {
                 int money = Integer.parseInt(st.nextToken());
                 int count = Integer.parseInt(st.nextToken());
 
+                coin[i] = money;
+                num[i] = count;
                 sum += (money * count);
+            }
 
+            for(int i = 1 ; i <= n ; i++) {
+                int money = coin[i];
+                int count = num[i];
 
-                for (int k = 500_00; k >= money; k--) {
+                for (int k = sum / 2; k >= money; k--) {
                     if(dp[k - money]) {
                         for (int j = 0; j < count; j++) {
                             if(k + (money * j) <= 500_00) {
@@ -38,7 +49,6 @@ public class Main {
                         }
                     }
                 }
-
             }
 
             int result = 0;
