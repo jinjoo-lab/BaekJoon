4706efccc27a84fa78a76df20efb9893b3d76e5b
진주원(JooWon Jin) <84346055+jinjoo-lab@users.noreply.github.com>
2024-09-05 12:30:24 +0900

    [Gold II] Title: 동전 분배, Time: 1824 ms, Memory: 17160 KB -BaekjoonHub

--- 백준/Gold/1943. 동전 분배/동전 분배.java
+++ 백준/Gold/1943. 동전 분배/동전 분배.java
@@ -1,61 +1,56 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    public static void main(String[] args) throws IOException {
-        String line = null;
-        while((line = br.readLine())!= null){
-            dp(line);
-        }
-        bw.flush();
-        bw.close();
-    }
+    static int n;
+    static boolean[] dp = new boolean[50_001];
 
-    private static void dp(String line) throws IOException {
-        int N = Integer.parseInt(line);
-        Queue<Node> pq = new LinkedList<>();
-        int sum = 0;
-        for (int i = 0 ; i < N ; i++) {
-            int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-            pq.add(new Node(input[0],input[1]));
-            sum += input[0] * input[1];
-        }
-        if (sum % 2 != 0) {
-            bw.write(0+"\n");
-            return;
-        }
-        boolean[] dp = new boolean[sum/2+1];
-        dp[0] = true;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
 
-        while (!pq.isEmpty()) {
-            Node cur = pq.poll();
-            for (int i = sum/2 ; i >= cur.v ; i--) {
-                if (i == cur.v || dp[i-cur.v]) {
-                    for(int k=0;k<=cur.c;k++){
-                        if(i + (k*cur.v) <= sum/2){
-                            dp[i + (k*cur.v)] = true;
-                            if ((i + (k*cur.v)) == sum/2) {
-                                bw.write(1+"\n");
-                                return;
+
+        for(int tt = 1 ; tt <= 3 ; tt++) {
+            dp = new boolean[50_001];
+            n = Integer.parseInt(br.readLine());
+            dp[0] = true;
+
+            int sum = 0;
+
+            for (int i = 1; i <= n; i++) {
+                st = new StringTokenizer(br.readLine(), " ");
+
+                int money = Integer.parseInt(st.nextToken());
+                int count = Integer.parseInt(st.nextToken());
+
+                sum += (money * count);
+
+
+                for (int k = 500_00; k >= money; k--) {
+                    if(dp[k - money]) {
+                        for (int j = 0; j < count; j++) {
+                            if(k + (money * j) <= 500_00) {
+                                dp[k + (money * j)] = dp[k + (money * j)] || dp[k + (money * j) - money];
                             }
                         }
                     }
                 }
+
             }
+
+            int result = 0;
+            if(sum % 2 == 0) {
+                result = dp[sum / 2] ? 1 : 0;
+            }
+
+            sb.append(result + "\n");
         }
 
-        bw.write(0+"\n");
-    }
-}
-class Node{
-    int v;
-    int c;
-
-    Node(int v,int c){
-        this.v = v;
-        this.c = c;
+        bw.write(sb.toString());
+        br.close();
+        bw.close();
     }
 }
