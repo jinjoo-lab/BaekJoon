61a389d57a6c42972f836c0437d31963b00b2422
진주원(JooWon Jin) <84346055+jinjoo-lab@users.noreply.github.com>
2024-09-08 18:08:43 +0900

    [Gold III] Title: 코딩은 예쁘게, Time: 72 ms, Memory: 11880 KB -BaekjoonHub

--- 백준/Gold/2879. 코딩은 예쁘게/코딩은 예쁘게.java
+++ 백준/Gold/2879. 코딩은 예쁘게/코딩은 예쁘게.java
@@ -2,106 +2,103 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int n = 0;
-    static int[] sarr;
-    static int[] last;
 
-    public static void main(String[] args) throws IOException {
+    static int n;
+    static int[] start;
+    static int[] end;
+    static int[] minus;
+
+    static int result = 0;
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        sarr = new int[n+1];
-        last = new int[n+1];
-        int[] dif = new int[n+1];
-
         StringTokenizer st = new StringTokenizer(br.readLine()," ");
-        for(int i=1;i<=n;i++)
-        {
-            sarr[i] = Integer.parseInt(st.nextToken());
+
+        n = Integer.parseInt(st.nextToken());
+
+        start = new int[n+1];
+        end = new int[n+1];
+        minus = new int[n+1];
+
+        st = new StringTokenizer(br.readLine()," ");
+        for(int i = 1 ; i <= n ; i++) {
+            start[i] = Integer.parseInt(st.nextToken());
         }
 
         st = new StringTokenizer(br.readLine()," ");
-        for(int i=1;i<=n;i++)
-        {
-            last[i] = Integer.parseInt(st.nextToken());
+        for(int i = 1 ; i <= n ; i++) {
+            end[i] = Integer.parseInt(st.nextToken());
 
-            dif[i] = last[i] - sarr[i];
+            minus[i] = end[i] - start[i];
         }
 
-        int result = 0;
-        boolean keep = true;
-        while(keep)
-        {
-            int over = over(dif[1]);
-            int plus = Math.abs(dif[1]);
-            int past = 1;
-
-            for(int i=2;i<=n;i++)
-            {
-                int tover  = over(dif[i]);
-
-                if(tover == over)
-                {
-                    plus = Math.min(plus, Math.abs(dif[i]));
-                }
-
-                else{
-                    result += plus;
-                    for(int j = past; j<=i-1;j++)
-                    {
-                        if(over == 1)
-                            dif[j] -=plus;
-
-                        else if(over == -1)
-                            dif[j] +=plus;
-                    }
-                    plus = Math.abs(dif[i]);
-                    over = tover;
-                    past = i;
-                }
-            }
-            for(int j= past;j<=n;j++)
-            {
-                if(over == 1)
-                    dif[j] -=plus;
-
-                else if(over == -1)
-                    dif[j] +=plus;
-            }
-            result += plus;
-            keep = keeping(dif);
+        while(!isIt()) {
+            go();
         }
-
         System.out.println(result);
 
         br.close();
     }
-    static void print(int[] dif)
-    {
-        for(int i=1;i<=n;i++)
-        {
-            System.out.print(dif[i]+" ");
+
+    static void print() {
+        for(int i= 1 ; i<= n ; i++) {
+            System.out.print(minus[i]+" ");
         }
         System.out.println();
     }
 
-    static boolean keeping(int[] dif)
-    {
-        for(int i=1;i<=n;i++)
-        {
-            if(dif[i] !=0)
-                return true;
+    static boolean isIt() {
+        for(int i = 1 ; i <= n ; i++) {
+            if(minus[i] != 0)
+                return false;
         }
 
-        return false;
+        return true;
     }
-    static int over(int cur)
-    {
-        if(cur > 0)
-            return 1;
 
-        else if(cur == 0)
+    static void go() {
+        int past = 1;
+        int go = minus[1];
+        int idx = 1;
+        int w = skrr(1);
+
+        while(idx <= n) {
+            int tmpW = skrr(idx);
+
+            if(w == tmpW) {
+                if(w == 1) {
+                    go = Math.min(go, minus[idx]);
+                }else if(w == -1) {
+                    go = Math.max(go, minus[idx]);
+                }
+                idx++;
+            }
+
+            else {
+                if(w == 1 || w == -1) {
+                    for(int i = past ; i <= idx - 1 ; i++) {
+                        minus[i] -= go;
+                    }
+                    result += Math.abs(go);
+                }
+                w = tmpW;
+                go = minus[idx];
+                past = idx;
+                idx++;
+            }
+        }
+
+        for(int i = past ; i <= idx - 1 ; i++) {
+            minus[i] -= go;
+        }
+        result += Math.abs(go);
+    }
+
+    static int skrr(int idx) {
+        if(minus[idx] == 0)
             return 0;
-
+        else if(minus[idx] >= 1)
+            return 1;
         else
             return -1;
     }
