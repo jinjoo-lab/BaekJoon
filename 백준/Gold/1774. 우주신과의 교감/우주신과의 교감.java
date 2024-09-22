6bb89d170e909c8267d58d9f63d29fae59b97a98
진주원(JooWon Jin) <84346055+jinjoo-lab@users.noreply.github.com>
2024-09-22 16:52:57 +0900

    [Gold III] Title: 우주신과의 교감, Time: 604 ms, Memory: 42500 KB -BaekjoonHub

--- 백준/Gold/1774. 우주신과의 교감/우주신과의 교감.java
+++ 백준/Gold/1774. 우주신과의 교감/우주신과의 교감.java
@@ -1,133 +1,115 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    static int n = 0;
-    static int m = 0;
+
+    static int n,m;
     static int[] root;
-    static int result = 0;
+    static double[][] graph;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        n = Integer.parseInt(st.nextToken());
-        root = new int[n+1];
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
 
-        for(int i=1;i<=n;i++)
-        {
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        graph = new double[n+1][2];
+
+        root = new int[n+1];
+        for(int i = 1 ; i <= n ; i++) {
             root[i] = i;
         }
 
-        m = Integer.parseInt(st.nextToken());
+        for(int i = 1 ; i <= n ; i++) {
+            st = new StringTokenizer(br.readLine()," ");
 
-        point[] parr = new point[n+1];
+            double x = Double.parseDouble(st.nextToken());
+            double y = Double.parseDouble(st.nextToken());
 
-        for(int i=1;i<=n;i++)
-        {
-            st = new StringTokenizer(br.readLine(), " ");
+            graph[i][0] = x;
+            graph[i][1] = y;
+        }
+
+        int count = n - 1;
+
+        for(int i = 1 ; i <= m ; i++) {
+            st = new StringTokenizer(br.readLine()," ");
+
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
 
-            parr[i] = new point(x,y);
+            union(x,y);
+
+            count--;
         }
 
-        for(int i=1;i<=m;i++)
-        {
-            st = new StringTokenizer(br.readLine(), " ");
-            int tmp1 = Integer.parseInt(st.nextToken());
-            int tmp2 = Integer.parseInt(st.nextToken());
-
-            union(tmp1, tmp2);
-        }
-
-
-        PriorityQueue<Edge> pq = new PriorityQueue<>(
-                (x,y) -> Double.compare(x.v,y.v)
+        PriorityQueue<Node> pq = new PriorityQueue<>(
+                (x,y) -> Double.compare(x.dis,y.dis)
         );
 
-        for(int i=1;i<n;i++)
-        {
-            for(int j=i+1;j<=n;j++)
-            {
-                   pq.add(new Edge(i,j,distance(parr[i], parr[j])));
+        for(int i = 1 ; i <= n ; i++) {
+            for(int j = i + 1 ; j <= n ; j++) {
+                pq.add(new Node(i,j,calDis(i,j)));
             }
         }
 
+        double result = 0;
 
-        kruskal(pq);
+        while(!pq.isEmpty()) {
+            Node cur = pq.poll();
+            
+
+            if(find(cur.v1) != find(cur.v2)) {
+                union(cur.v1, cur.v2);
+                result += cur.dis;
+                count--;
+            }
+        }
+
+        System.out.printf("%.2f",result);
+
 
 
         br.close();
     }
 
-    static void kruskal(PriorityQueue<Edge> pq)
-    {
-        double result = 0.0;
+    static double calDis(int idx1, int idx2) {
+        double x1 = graph[idx1][0];
+        double y1 = graph[idx1][1];
+        double x2 = graph[idx2][0];
+        double y2 = graph[idx2][1];
 
-        while(!pq.isEmpty())
-        {
-            Edge cur = pq.poll();
-
-            if(find(cur.x) != find(cur.y))
-            {
-                result += cur.v;
-                union(cur.x , cur.y);
-            }
-        }
-
-        System.out.printf("%.2f\n",result);
+        return Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
     }
 
-    static double distance(point a,point b)
-    {
-        double tmp1 = Math.pow(Math.abs((a.x - b.x)),2);
-        double tmp2 = Math.pow(Math.abs((a.y - b.y)),2);
+    static int find(int x) {
+        if(x == root[x])
+            return root[x];
 
-        double tmp3 = Math.sqrt(Math.abs(tmp2 + tmp1));
-
-        return tmp3;
+        return root[x] = find(root[x]);
     }
 
-    static int find(int x)
-    {
-        if(root[x] == x)
-            return x;
-        else
-            return root[x] = find(root[x]);
-    }
-
-    static void union(int x,int y)
-    {
+    static void union(int x,int y) {
         x = find(x);
         y = find(y);
 
         if(x < y)
             root[y] = x;
+
         else
             root[x] = y;
     }
-}
-class point
-{
-    int x;
-    int y;
 
-    point(int x,int y)
-    {
-        this.x = x;
-        this.y = y;
-    }
-}
-class Edge
-{
-    int x;
-    int y;
-    double v;
+    static class Node {
+        int v1;
+        int v2;
+        double dis;
 
-    Edge(int x,int y,double v)
-    {
-        this.x = x;
-        this.y = y;
-        this.v = v;
+        Node(int v1,int v2,double dis) {
+            this.v1 = v1;
+            this.v2 = v2;
+            this.dis = dis;
+        }
     }
 }
