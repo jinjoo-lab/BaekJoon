a215623cda8a996e00c8eced707c517fc3524f97
진주원(JooWon Jin) <84346055+jinjoo-lab@users.noreply.github.com>
2024-10-09 15:52:10 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 111.99 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
@@ -3,8 +3,8 @@ import java.util.*;
 class Solution {
     
     static int N;
-    static int rl = -1;
-    static int rr = -1;
+    static int rl = 1;
+    static int rr = 0;
     
     static HashMap<String,Integer> map = new HashMap<>();
     static HashMap<String,Integer> map2 = new HashMap<>();
@@ -16,6 +16,7 @@ class Solution {
         int[] answer = {};
         gemsInt = new int[gems.length + 1];
         int num = 1;
+        rr = gems.length;
         
         for(int i = 1 , size = gems.length ; i <= size ; i++) {
             if(map.containsKey(gems[i - 1])) {
@@ -29,10 +30,7 @@ class Solution {
     
         N = map.size();
         
-        go1(gems);
-        print(rl,rr);
-        
-        go2(gems);
+
         
         go3(gems);
         
@@ -43,73 +41,12 @@ class Solution {
         return answer;
     }
     
-    static void go2(String[] gems) {
-        int l = 1;
-        int r = gems.length;
-        
-        while(true) {
-            if(map2.get(gems[l - 1]) > 1) {
-                map2.put(gems[l - 1], map2.get(gems[l - 1]) - 1);
-                l++;
-            } else {
-                break;
-            }
-        }
-        
-        while(true) {
-            if(map2.get(gems[r - 1]) > 1) {
-                map2.put(gems[r - 1], map2.get(gems[r - 1]) - 1);
-                r--;
-            }else {
-                break;
-            }
-        }
-        
-        print(l,r);
-        
-        if(Math.abs(r - l) < Math.abs(rr - rl)) {
-            rl = l;
-            rr = r;
-        }
-    }
-    
-    static void go1(String[] gems) {
-        int l = 1;
-        int r = gems.length;
-        
-        while(true) {
-            if(map.get(gems[r - 1]) > 1) {
-                map.put(gems[r - 1], map.get(gems[r - 1]) - 1);
-                r--;
-            }else {
-                break;
-            }
-        }
-        
-        
-        while(true) {
-            if(map.get(gems[l - 1]) > 1) {
-                map.put(gems[l - 1], map.get(gems[l - 1]) - 1);
-                l++;
-            } else {
-                break;
-            }
-        }
-        
-        rl = l;
-        rr = r;
-        
-    }
-    
-    static void print(int l,int r){
-        System.out.println(l+" "+r);        
-    }
+
     
     static void go3(String[] gems) {
         HashMap<String,Integer> map = new HashMap<>();
-        map.put(gems[0], 1);
         int l = 1;
-        int r = 2;
+        int r = 1;
         
         while(l <= r && r <= gems.length) {
             if(map.containsKey(gems[r - 1])) {
